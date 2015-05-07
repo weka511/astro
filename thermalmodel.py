eac40091b8d84d28da69728b5e411720784d6811
Simon Crase <simon@greenweaves.nz>
2015-05-07 21:14:51 +1200

    Calculate temperature gain in surface

--- thermalmodel.py
+++ thermalmodel.py
@@ -17,7 +17,7 @@ import math, planet, solar
 
 class Layer:
     next_id=0
-    def __init__(self,name,latitude,longitude,thickness,depth):
+    def __init__(self,name,latitude,longitude,thickness,depth,planet):
         self.id=Layer.next_id
         Layer.next_id+=1
         self.name=name
@@ -27,12 +27,13 @@ class Layer:
         self.thickness = thickness
         self.top_temperature = float('NaN')
         self.bottom_temperature = float('NaN')
+        self.planet = planet
         
     def inititialize_temperatures(self,t0):
         self.top_temperature = t0
         self.bottom_temperature = t0    
     
-    def propagate_temperature(self,above,areocentric_longitude,T):
+    def propagate_temperature(self,above,areocentric_longitude,T,dT):
         return self
     
     def __str__(self):
@@ -52,36 +53,53 @@ class Layer:
 
 
 class MedialLayer(Layer):
-    def __init__(self,latitude,longitude,thickness,depth):
-        Layer.__init__(self,"Medial",latitude,longitude,thickness,depth)
+    def __init__(self,latitude,longitude,thickness,depth,planet):
+        Layer.__init__(self,"Medial",latitude,longitude,thickness,depth,planet)
         
 class Surface(Layer):
     stefan_bolzmann = 5.670374e-8
-    def __init__(self,latitude,longitude,solar):
-        Layer.__init__(self,"Surface",latitude,longitude,0,0)
+    
+    def __init__(self,latitude,longitude,thickness,solar,planet):
+        Layer.__init__(self,"Surface",latitude,longitude,thickness,0,planet)
         self.solar=solar
         
-    def propagate_temperature(self,above,areocentric_longitude,T):
-        previous= Layer.propagate_temperature(self,above,areocentric_longitude,T)
-        irradiance=solar.surface_irradience(areocentric_longitude,self.latitude,T)
+    def propagate_temperature(self,above,areocentric_longitude,T,dT):
+        previous= Layer.propagate_temperature(self,above,areocentric_longitude,T,dT)
+        irradiance=self.planet.F*solar.surface_irradience(areocentric_longitude,self.latitude,T)
         t2=self.top_temperature*self.top_temperature
-        outflow=Surface.stefan_bolzmann*t2*t2
+        outflow=self.planet.e*Surface.stefan_bolzmann*t2*t2
         nett_gain = irradiance - outflow
         print "Irradiance ={0:6.1f}, outflow={1:6.1f}, nett_gain={2:6.1f}".format(irradiance,outflow,nett_gain)
+        heat = nett_gain*dT
+        delta_temperature= heat / (self.planet.C * self.planet.rho * self.thickness)
+        self.top_temperature += delta_temperature
+        self.bottom_temperature += delta_temperature
+        print ( \
+            "dT={0:4.2f}, "+\
+            "heat={1:6.4f}, "+\
+            "dTemp={2:6.4g}, "+ \
+            "Temp= {3:12.6f}"\
+            ).format( \
+                  dT, \
+                  heat, \
+                  delta_temperature, \
+                  0.5*(self.top_temperature+self.bottom_temperature) \
+            )
         return previous
     
 class Bottom(Layer):
     def __init__(self,layer):
-        Layer.__init__(self,"Bottom",layer.latitude,layer.longitude,layer.thickness,layer.depth)
+        Layer.__init__(self,"Bottom",layer.latitude,layer.longitude,layer.thickness,layer.depth,layer.planet)
         
 class ThermalModel:
-    def __init__(self,latitude,longitude,spec,solar):
+    def __init__(self,latitude,longitude,spec,solar,planet):
         self.layers=[]
-        z=0
-        self.layers.append(Surface(latitude,longitude,solar))
+        (n,dz)=spec[0]
+        z=dz
+        self.layers.append(Surface(latitude,longitude,dz,solar,planet))
         for (n,dz)in spec:
             for i in range(n):
-                self.layers.append(MedialLayer(latitude,longitude,dz,z))
+                self.layers.append(MedialLayer(latitude,longitude,dz,z,planet))
                 z+=dz
         bottom=self.layers.pop()
         self.layers.append(Bottom(bottom))
@@ -90,10 +108,10 @@ class ThermalModel:
         for layer in self.layers:
             layer.inititialize_temperatures(t0)
             
-    def propagate_temperature(self,areocentric_longitude,T):
+    def propagate_temperature(self,areocentric_longitude,T,dT):
         above=None
         for layer in self.layers:
-            above=layer.propagate_temperature(above,areocentric_longitude,T)
+            above=layer.propagate_temperature(above,areocentric_longitude,T,dT)
 
 
              
@@ -103,17 +121,21 @@ if __name__=="__main__":
     mars = planet.Mars()
     solar = solar.Solar(mars)
         
-    thermal=ThermalModel(22.3,0,[(9,0.015),(10,0.3)],solar)
+    thermal=ThermalModel(22.3,0,[(9,0.015),(10,0.3)],solar,mars)
     thermal.inititialize_temperatures(150)
     
     #for layer in thermal.layers:
         #print layer
      
-    thermal.propagate_temperature(153,12)
+    n=10
+    for day in range(720):
+        for hour in range(24):
+            for i in range(n):
+                thermal.propagate_temperature(153,hour,1.0/n)
     
-    thermal.propagate_temperature(153,18)
+#    thermal.propagate_temperature(153,18, 0.1)
     
-    thermal.propagate_temperature(153,20)
+#    thermal.propagate_temperature(153,20, 0.1)
     
     #for layer in thermal.layers:
         #print layer    
