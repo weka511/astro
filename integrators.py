ae6a55f3aa20033a21239f9b7e0cbaad8b3de673
Simon Crase <simon@greenweaves.nz>
2015-06-06 13:18:19 +1200

    Split Kepler Hamiltonian and Integrator into separate classes

---
+++ integrators.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2015 Greenweaves Software Pty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>
+
+class Integrate2:
+    def __init__(self,h,hamiltonian):
+        self.h=h
+        self.hamiltonian=hamiltonian
+        
+    def predict(self):
+        return [x0 + self.h * f0 for (x0,f0) in zip(self.hamiltonian.x,self.hamiltonian.dx())]    
+    
+    def correct(self,hamiltonian1):
+        return [e0 + (self.h/2) * (f0 + f1) for (e0,f0,f1) in zip(self.hamiltonian.eta,self.hamiltonian.d_eta(),hamiltonian1.d_eta())]
+    
+    def integrate(self):
+        hamiltonian1=self.hamiltonian.create(self.predict())
+        self.hamiltonian.eta=self.correct(hamiltonian1)
+        self.hamiltonian.invert(hamiltonian1)
+        
--- kepler.py
+++ kepler.py
@@ -13,52 +13,58 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-import math, utilities
+import math, utilities, integrators
 
-def predict(h,f,x):
-    #print x
-    #print f(x)
-    return [x0 + h * f0 for (x0,f0) in zip(x,f(x))]
-
-def eta(x,m,k):
-    return [
-        -k/x[0],
-        x[2]*x[2]/(2*m) + x[3]*x[3]/(2*m*x[0]*x[0]),
-        x[3]
-    ]
-
-def correct(h,f,eta0,eta1,x0,x1):
-    return [e0 + (h/2) * (f0 + f1) for (e0,f0,f1) in zip(eta0,f(eta0,x0),f(eta1,x1))]
-
-
+class Kepler:
+    def __init__(self,x,m,k):
+        self.x = x
+        self.m = m
+        self.k = k
+        self.transform()
+        
+    def create(self,x):
+        return Kepler(x,self.m,self.k)
     
-def inverse(eta,m,k,x):
-    r=-k/eta[0]
-    L=eta[2]
-    psq=2*m*eta[1]-L*L/(r*r)
-    if psq>0:
-        p=utilities.signum(x[2])*math.sqrt(2*m*eta[1]-L*L/(r*r))
-    else:
-        p=0
-    return [r, x[1], p, L]
+    def transform(self):
+        self.eta=[
+            -self.k/self.x[0],
+            self.x[2]*self.x[2]/(2*self.m) + 
+            self.x[3]*self.x[3]/(2*self.m*self.x[0]*self.x[0]),
+            x[3]
+        ]
+    
+    def invert(self,kepler):
+        r=-self.k/self.eta[0]
+        L=self.eta[2]
+        psq=2*self.m*self.eta[1]-L*L/(r*r)        
+        p=utilities.signum(x[2])*math.sqrt(psq) if psq>0 else 0
+        self.x[0] = r
+        self.x[1] = kepler.x[1]
+        self.x[2] = p
+        self.x[3] = L
 
-def kepler_pred(x,m,k): # r, theta, p, l
-    return [
-        x[2]/m, # p/m
-        x[3]/(m*x[0]*x[0]), # l/(m*r*r)
-        x[3]*x[3]/(m*x[0]*x[0]*x[0]) - k / (x[0]*x[0]),
-        0
-    ]
+    def dx(self):
+        return [
+            self.x[2]/self.m, # p/m
+            self.x[3]/(self.m*self.x[0]*self.x[0]), # l/(m*r*r)
+            self.x[3]*self.x[3]/(self.m*self.x[0]*self.x[0]*self.x[0]) - self.k / (self.x[0]*self.x[0]),
+            0
+        ]    
 
-def kepler_corr(eta,m,k,x):
-    term=k*x[2]/(m*x[0]*x[0])
-    return [term,-term,0]
+    def d_eta(self):
+        term=self.k*self.x[2]/(self.m*self.x[0]*self.x[0])
+        return [term,-term,0]
 
-def hamiltonian(x,m,k):
-    return x[2]*x[2]/(2*m) + x[3]*x[3]/(2*m*x[0]*x[0])-k/x[0]
+    def hamiltonian(self):
+        return self.x[2]*self.x[2]/(2*self.m) + self.x[3]*self.x[3]/(2*self.m*self.x[0]*self.x[0])-self.k/self.x[0]
 
-def ff(eta,x):
-    return kepler_corr(eta,m,k,x)
+    def display(self):
+        print "x",self.x
+        print "eta",self.eta
+        
+
+
+
 
 if __name__=='__main__':
     import matplotlib.pyplot as plt
@@ -73,23 +79,17 @@ if __name__=='__main__':
     m = 0.001
     L = math.sqrt((m/r*r*r ))
     x=[r,0,p,L]
-    #print x
-    nn=10000
+    kepler=Kepler(x,m,k)
+    nn=100000
     step = 1
-    print hamiltonian(x,m,k)
-     
-    for i in range(nn):
-        x1 = predict(h,lambda(z) : kepler_pred(z,m,k),x)
-        eta0=eta(x,m,k)
-        eta1=eta(x1,m,k)
-        eta2=correct(h,ff,eta0,eta1,x,x1)
-        x=inverse(eta2,m,k,x1)
-        #print x
-        if i%step==0:
-            w.append(x1[0]*math.cos(x1[1]))
-            z.append(x1[0]*math.sin(x1[1]))            
-            u.append(x[0]*math.cos(x[1]))
-            v.append(x[0]*math.sin(x[1]))
-    plt.plot(w,z,'b',u,v,'r')
+    print kepler.hamiltonian()
+    integrator = integrators.Integrate2(h,kepler)
     
-    print hamiltonian(x,m,k)
+    for i in range(nn):
+        integrator.integrate()
+        u.append(kepler.x[0]*math.cos(kepler.x[1]))
+        v.append(kepler.x[0]*math.sin(kepler.x[1]))
+    plt.plot(u,v)
+  
+    
+    print kepler.hamiltonian()
