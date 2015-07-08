4e2c641abf6a75da039b3f4c043cedc050fa53b0
Simon Crase <simon@greenweaves.nz>
2015-07-09 10:03:22 +1200

    Move tracking into windows temp directory

---
+++ plot_points.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2015 Greenweaves Software Pty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>
+
+if __name__=='__main__':
+    import tracking, matplotlib.pyplot as plt
+    log=tracking.Tracking('run')
+    x1s=[]
+    y1s=[]
+    x2s=[]
+    y2s=[]    
+    x3s=[]
+    y3s=[]
+    sampling_period=1
+    for name in log.get_reader():
+        print name
+        xs = log.get_one_status(name)
+        i=0
+        while 12*i<len(xs):
+            if i%sampling_period==0:
+                x1s.append(xs[12*i])
+                y1s.append(xs[12*i+1])
+                x2s.append(xs[12*i+2])
+                y2s.append(xs[12*i+3])        
+                x3s.append(xs[12*i+4])
+                y3s.append(xs[12*i+5])
+            i+=1
+    plt.plot( x3s,y3s,'ro', x1s,y1s,'bo', x2s,y2s,'go')  
--- tracking.py
+++ tracking.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-import os, re
+import os, re, tempfile
 from array import array
 
 class Reader(object):
@@ -35,7 +35,7 @@ class Reader(object):
             raise StopIteration
     
 class Tracking(object):
-    def __init__(self,path,header,suffix='dat'):
+    def __init__(self,header,suffix='dat',path=tempfile.gettempdir()):
         self.latest = ("",-1)
         self.header = header
         self.path   = path
@@ -78,75 +78,49 @@ class Tracking(object):
             return []
 
 if __name__=='__main__':
-    import math, restricted,rki, matplotlib.pyplot as plt
-    tracking=Tracking('c:\\temp','run')
-    x1s=[]
-    y1s=[]
-    x2s=[]
-    y2s=[]    
-    x3s=[]
-    y3s=[]
-    sampling_period=100
-    for name in tracking.get_reader():
-        print name
-        xs = tracking.get_one_status(name)
-        i=0
-        while len(xs)>0:
-            x=xs[0:12]
-            xs=xs[12:]
-            if i%sampling_period==0:
-                x1s.append(x[0])
-                y1s.append(x[1])
-                x2s.append(x[2])
-                y2s.append(x[3])        
-                x3s.append(x[4])
-                y3s.append(x[5])
-            i+=1
-    plt.plot( x3s,y3s,'r', x1s,y1s,'b', x2s,y2s,'g')        
+    import math, restricted,rki
+    tracking=Tracking('run')
         
-        #import math, restricted,rki
-        #tracking=Tracking('c:\\temp','run')
-        
-    #old_trace= tracking.get_status()
+    old_trace= tracking.get_status()
     
-    #nn=1000
-    #h = 0.001
-    #M1 = 250000
-    #epsilon=0.1
-    #M2 = 1.0
-    #G = 1.0
-    #L = 25.0
-    #R2 = L*M1/(M1+M2)
-    #x2 = R2
-    #y2 = 0
-    #x1 = -L*M2/(M1+M2)
-    #y1 = 0.0
-    #x3 = 0.5*(x1+x2)*(1+epsilon)
-    #y3 = L* math.sqrt(3.0)/2.0
-    #omega = math.sqrt(G*(M1+M2)/(L*L*L))
-    #x=[]
-    #if len(old_trace)==0:
-        #x=[
-            #x1, y1,
-            #x2, y2,
-            #x3, y3, 
-            #0, x1*omega,
-            #0, x2*omega,
-            #-0.5*math.sqrt(3)*L*omega, 0.5*L*omega
-        #]
-    #else:
-        #for i in range(12):
-            #x.append(old_trace[len(old_trace)-12+i])
+    nn=100000
+    h = 0.001
+    M1 = 250000
+    epsilon=0.1
+    M2 = 1.0
+    G = 1.0
+    L = 25.0
+    R2 = L*M1/(M1+M2)
+    x2 = R2
+    y2 = 0
+    x1 = -L*M2/(M1+M2)
+    y1 = 0.0
+    x3 = 0.5*(x1+x2)*(1+epsilon)
+    y3 = L* math.sqrt(3.0)/2.0
+    omega = math.sqrt(G*(M1+M2)/(L*L*L))
+    x=[]
+    if len(old_trace)==0:
+        x=[
+            x1, y1,
+            x2, y2,
+            x3, y3, 
+            0, x1*omega,
+            0, x2*omega,
+            -0.5*math.sqrt(3)*L*omega, 0.5*L*omega
+        ]
+    else:
+        for i in range(12):
+            x.append(old_trace[len(old_trace)-12+i])
 
-    #trace=[]
+    trace=[]
     
-    #rk = rki.ImplicitRungeKutta4(lambda (x): restricted.dx(x,1,M1,M2),200,1e-18)
+    rk = rki.ImplicitRungeKutta4(lambda (x): restricted.dx(x,1,M1,M2),200,1e-18)
     
-    #driver = rki.Driver(rk,1.e-8,0.5,1.0,1e-12)
+    driver = rki.Driver(rk,1.e-8,0.5,1.0,1e-12)
     
-    #trace=trace+x
-    #for i in range(nn):
-        #x= driver.step(x)
-        #trace=trace+x
-    #tracking.output_status(trace)
+    trace=trace+x
+    for i in range(nn):
+        x= driver.step(x)
+        trace=trace+x
+    tracking.output_status(trace)
     
