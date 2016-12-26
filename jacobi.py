4ebe02c9291d7a9c76c6f222df4469e479aeae5c
Simon Crase <simon@greenweaves.nz>
2016-12-26 14:54:35 +1300

    Fill in space with colour

--- jacobi.py
+++ jacobi.py
@@ -1,39 +1,46 @@
 import numpy as np, matplotlib.pyplot as plt, math as m 
 
-origin = 'lower'
 
 @np.vectorize
 def jacobi(x,y,n=1,mu2=0.2,Cj=3.9):
     n2  = n*n
     mu1 = 1-mu2
-    x2 = x*x
-    y2 = y*y
-    r1  = m.sqrt((x+mu2)*(x+mu2)+y2)
-    r2  = m.sqrt((x-mu1)*(x-mu1)+y2)
-    diff= n2*(x2 + y2) + 2*(mu1/r1 + mu2/r2)-Cj
-    return clip(diff)
-
-def clip(diff,limit=1):
-    if diff>limit:
-        return limit
-    if diff<-limit:
-        return -limit
+    x2  = x*x
+    y2  = y*y
+    r1  = m.sqrt((x+mu2)*(x+mu2) + y2)
+    r2  = m.sqrt((x-mu1)*(x-mu1) + y2)
+    diff= n2*(x2 + y2) + 2*(mu1/r1 + mu2/r2) - Cj
+ 
     return diff
 
+def bounds(Z):
+    minZ = float('inf')
+    maxZ = - minZ
+    for zz in Z:
+        for z in zz:
+            if z < minZ:
+                minZ = z
+            if z > maxZ:
+                maxZ = z
+    return (minZ,maxZ)     
+
 plt.figure()
-xlist = np.linspace(-20.0, 20.0, 100)   
-ylist = np.linspace(-20.0, 20.0, 100)
+limit=5
+xlist = np.linspace(-limit, limit, 100)   
+ylist = np.linspace(-limit, limit, 100)
 X,Y = np.meshgrid(xlist, ylist)   
 Z = jacobi(X,Y)
+(z0,z1) = bounds(Z)
+print (z0,z1)
+#levels=np.linspace(z0,z1,num=21,endpoint=True)
+levels=list(range(-2,0,10))+list(range(0,60,10))
+c = plt.pcolor(X,Y,Z)
+d = plt.colorbar(c,orientation='horizontal')
+#q = plt.winter()
 
-levels=np.linspace(-0.5,0.5,num=21,endpoint=True)
-CS3=plt.contour(X, Y, Z, 
-            levels,
-            colors=[ 'r','g','b'],
-            #linestyles = 'solid',
-            #origin=origin,
-            #extend='both'
-            )
+CS3=plt.contourf(X, Y, Z, levels, colors=[ 'g'] )
+plt.xlabel('x')
+plt.ylabel('y')
 plt.clabel(CS3)
 CS3.cmap.set_under('m')
 CS3.cmap.set_over('c')
