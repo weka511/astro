e6a23e242bf3c9b51c28ee8ba4e96b74565c473a
Simon Crase <simon@greenweaves.nz>
2016-12-26 15:56:55 +1300

    Replicate plots from Nurrar & Dermott

--- jacobi.py
+++ jacobi.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2015 Greenweaves Software Pty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>
 import numpy as np, matplotlib.pyplot as plt, math as m 
 
 
@@ -9,9 +23,7 @@ def jacobi(x,y,n=1,mu2=0.2,Cj=3.9):
     y2  = y*y
     r1  = m.sqrt((x+mu2)*(x+mu2) + y2)
     r2  = m.sqrt((x-mu1)*(x-mu1) + y2)
-    diff= n2*(x2 + y2) + 2*(mu1/r1 + mu2/r2) - Cj
- 
-    return diff
+    return  n2*(x2 + y2) + 2*(mu1/r1 + mu2/r2) - Cj
 
 def bounds(Z):
     minZ = float('inf')
@@ -24,25 +36,27 @@ def bounds(Z):
                 maxZ = z
     return (minZ,maxZ)     
 
-plt.figure()
-limit=5
-xlist = np.linspace(-limit, limit, 100)   
-ylist = np.linspace(-limit, limit, 100)
-X,Y = np.meshgrid(xlist, ylist)   
-Z = jacobi(X,Y)
-(z0,z1) = bounds(Z)
-print (z0,z1)
-#levels=np.linspace(z0,z1,num=21,endpoint=True)
-levels=list(range(-2,0,10))+list(range(0,60,10))
-c = plt.pcolor(X,Y,Z)
-d = plt.colorbar(c,orientation='horizontal')
-#q = plt.winter()
+def plot_jacobi(fig=1,n=1,mu2=0.2,Cj=3.9,limit=5): 
+    plt.figure(fig)
+    xlist = np.linspace(-limit, limit+0.001, 100)   
+    ylist = np.linspace(-limit, limit+0.001, 100)
+    X,Y = np.meshgrid(xlist, ylist)   
+    Z = jacobi(X,Y,n,mu2,Cj)
+    (z0,z1) = bounds(Z)
+    
+    levels=list(range(m.floor(z0),0,10))+list(range(0,m.ceil(z1),10))
+    
+    c = plt.pcolor(X,Y,Z)
+    d = plt.colorbar(c,orientation='horizontal')
+      
+    CS3=plt.contourf(X, Y, Z, levels)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(r'Zero velocity surfaces for $n={0},\mu_2 = {1},C_j={2}$'.format(n,mu2,Cj))
 
-CS3=plt.contourf(X, Y, Z, levels, colors=[ 'g'] )
-plt.xlabel('x')
-plt.ylabel('y')
-plt.clabel(CS3)
-CS3.cmap.set_under('m')
-CS3.cmap.set_over('c')
-#plt.colorbar(CS3)
-plt.show()
+
+if __name__=='__main__':
+    for i in range(1,50):
+        plot_jacobi(fig=i,n=1,mu2=0.2,Cj=0.0+i/10,limit=2)
+
+    plt.show()
