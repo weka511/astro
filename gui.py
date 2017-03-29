3f75b0a73530f3683f6d73facc4d0b058bd8d48f
Simon Crase <simon@greenweaves.nz>
2017-03-29 19:12:16 +1300

    Support multithreading in #14

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 Greenweaves Software Pty Ltd
+# Copyright (C) 2017 Greenweaves Software Pty Ltd
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,23 +17,25 @@
 User interface for Viewer
 
 Do not try to run this under the IDE, as it fights with multiprocessing
+
 '''
 
 import fnmatch,os,tkinter as tk,viewer,matplotlib.pyplot as plt,multiprocessing as mp
 from tkinter import *
 
-def exec_display_ext(inputfile,figure):
-    print(inputfile,figure)
+# The next few methods need to be declared before multiprocessing calls
+def exec_display_ext(inputfile,figure): 
+    print('Display {0} as figure {1}'.format(inputfile,figure))
     viewer.display(inputfile,figure=figure)         
     plt.show()
     
 def exec_display_maxmin_ext(inputfile,figure):
-    print(inputfile,figure)
+    print('Display Max Min{0} as figure {1}'.format(inputfile,figure))
     viewer.display_maxmin(inputfile,figure=figure)         
     plt.show() 
     
 def exec_display_daily_minima_ext(inputfile,figure):
-    print(inputfile,figure)
+    print('Display Daily {0} as figure {1}'.format(inputfile,figure))
     viewer.display_daily_minima_all_latitudes(figure=figure)         
     plt.show()     
     
@@ -43,6 +45,7 @@ class Viewer(tk.Frame):
         self.pack()
         self.createWidgets()
         self.figure=1
+        self.tasks=[]
         
     def createWidgets(self):
         scrollbar = tk.Scrollbar(root, orient="vertical")
@@ -69,29 +72,33 @@ class Viewer(tk.Frame):
         self.daily_button['command'] = self.exec_daily_minima
         self.daily_button.grid(row=1,column=2)         
 
-        self.QUIT = tk.Button(self, text='QUIT', fg="red", command=root.destroy)
+        self.QUIT = tk.Button(self, text='QUIT', fg="red", command=self.close_all)
         self.QUIT.grid(row=2,column=1) 
 
+    def close_all(self):
+        for task in self.tasks:
+            if task.is_alive():
+                task.terminate()
+                task.join()
+        root.destroy()
+        
     def exec_all(self):
         inputfile=self.file_list.get(self.file_list.curselection())
-        print (inputfile)
-        job_for_another_core = mp.Process(target=exec_display_ext,args=(inputfile,self.figure))
-        job_for_another_core.start()  
+        self.tasks.append(mp.Process(target=exec_display_ext,args=(inputfile,self.figure)))
+        self.tasks[-1].start()  
         self.figure+=1
  
         
     def exec_maxmin(self):
         inputfile=self.file_list.get(self.file_list.curselection())
-        print (inputfile)
-        job_for_another_core = mp.Process(target=exec_display_maxmin_ext,args=(inputfile,self.figure))
-        job_for_another_core.start() 
+        self.tasks.append(mp.Process(target=exec_display_maxmin_ext,args=(inputfile,self.figure)))
+        self.tasks[-1].start() 
         self.figure+=1
         
     def exec_daily_minima(self):
         inputfile=self.file_list.get(self.file_list.curselection())
-        print (inputfile)
-        job_for_another_core = mp.Process(target=exec_display_daily_minima_ext,args=(inputfile,self.figure))
-        job_for_another_core.start() 
+        self.tasks.append( mp.Process(target=exec_display_daily_minima_ext,args=(inputfile,self.figure)))
+        self.tasks[-1].start()
         self.figure+=1  
         
 
