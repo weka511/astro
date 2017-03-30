074e21dead65b2abed973d3f0c6904546ba3d06a
Simon Crase <simon@greenweaves.nz>
2017-03-31 07:02:55 +1300

    Added doco strings

--- gui.py
+++ gui.py
@@ -23,23 +23,34 @@ diplay them graphically.
 
 '''
 
-import fnmatch,os,tkinter as tk,viewer,matplotlib.pyplot as plt,multiprocessing as mp,re
+import fnmatch,os,viewer,matplotlib.pyplot as plt,multiprocessing as mp,re
 from tkinter import *
 
 # The next few methods need to be declared before multiprocessing calls
 # as they run as separate processes.
 
-def exec_display_ext(inputfile,figure): 
+def exec_display_ext(inputfile,figure):
+    '''
+    Execute the display function and show plot
+    '''
     print('Display {0} as figure {1}'.format(inputfile,figure))
     viewer.display(inputfile,figure=figure)         
     plt.show()
     
 def exec_display_maxmin_ext(inputfile,figure):
+    '''
+    Execute the display maxmin function and show plot
+    '''    
     print('Display Max Min{0} as figure {1}'.format(inputfile,figure))
     viewer.display_maxmin(inputfile,figure=figure)         
     plt.show() 
     
 def exec_display_daily_minima_ext(inputfile,figure):
+    '''
+    Execute the display all function and show plot
+    Use the selected file as a template, so we get all runs (all latitudes)
+    with other parameters matching.
+    '''    
     print('Display Daily {0} as figure {1}'.format(inputfile,figure))
     m=re.match('(^[0-9]+-[0-9]+-[0-9]+-.*co2-)([0-9]+[NS]?)(.txt)',inputfile)  
     pattern='{0}[0-9]+([NS])?{1}'.format(m.group(1),m.group(3))
@@ -47,12 +58,13 @@ def exec_display_daily_minima_ext(inputfile,figure):
     viewer.display_daily_minima_all_latitudes(figure=figure,pattern=pattern)         
     plt.show()     
 
-'''
-This class represents to Frame and UI elements
-'''
-class Viewer(tk.Frame):
+
+class Viewer(Frame):
+    '''
+    This class represents to Frame and UI elements
+    '''    
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
+        Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
         self.figure=1
@@ -60,7 +72,7 @@ class Viewer(tk.Frame):
 
         
     def createWidgets(self):
-        scrollbar = tk.Scrollbar(root, orient="vertical")
+        scrollbar = Scrollbar(root, orient="vertical")
         self.file_list=Listbox(self, width=50, yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.file_list.yview)
         
@@ -69,25 +81,29 @@ class Viewer(tk.Frame):
                 self.file_list.insert('end',file)        
  
         self.file_list.grid(row=0,column=1)
-        self.all_button = tk.Button(self)
+        self.all_button = Button(self)
         self.all_button['text'] = 'All'
         self.all_button['command'] = self.exec_all
         self.all_button.grid(row=1,column=0) 
         
-        self.minmax_button = tk.Button(self)
+        self.minmax_button = Button(self)
         self.minmax_button['text'] = 'MinMax'
         self.minmax_button['command'] = self.exec_maxmin
         self.minmax_button.grid(row=1,column=1) 
         
-        self.daily_button = tk.Button(self)
+        self.daily_button = Button(self)
         self.daily_button['text'] = 'Daily'
         self.daily_button['command'] = self.exec_daily_minima
         self.daily_button.grid(row=1,column=2)         
 
-        self.QUIT = tk.Button(self, text='QUIT', command=self.close_all)
-        self.QUIT.grid(row=2,column=1) 
+        self.close = Button(self, text='Close', command=self.close_all)
+        self.close.grid(row=2,column=1) 
+
 
     def close_all(self):
+        '''
+        When user presses Close, terminate all threads, then exit
+        '''        
         for task in self.tasks:
             if task.is_alive():
                 task.terminate()
@@ -95,6 +111,9 @@ class Viewer(tk.Frame):
         root.destroy()
         
     def exec_all(self):
+        '''
+        All button pressed
+        '''
         inputfile=self.file_list.get(self.file_list.curselection())
         self.tasks.append(mp.Process(target=exec_display_ext,
                                      args=(inputfile,self.figure)))
@@ -103,6 +122,9 @@ class Viewer(tk.Frame):
  
         
     def exec_maxmin(self):
+        '''
+        MaxMin button pressed
+        '''
         inputfile=self.file_list.get(self.file_list.curselection())
         self.tasks.append(mp.Process(target=exec_display_maxmin_ext,
                                      args=(inputfile,self.figure)))
@@ -110,6 +132,9 @@ class Viewer(tk.Frame):
         self.figure+=1
         
     def exec_daily_minima(self):
+        '''
+        Daily minima pressed
+        '''
         inputfile=self.file_list.get(self.file_list.curselection())
         self.tasks.append(mp.Process(target=exec_display_daily_minima_ext,
                                       args=(inputfile,self.figure)))
@@ -119,7 +144,7 @@ class Viewer(tk.Frame):
 
 if __name__=='__main__':   
     mp.freeze_support() # Need this for MS Windows
-    root = tk.Tk()
+    root = Tk()
     root.title('Leighton and Murray Viewer')
     app = Viewer(master=root)
     app.mainloop()
