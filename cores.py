f61d8c5fe13e0d7676d7da2c20d6bed3d634cd54
Simon Crase <simon@greenweaves.nz>
2017-03-30 14:15:41 +1300

    Untangled startup logic

--- cores.py
+++ cores.py
@@ -34,10 +34,10 @@ def work(item):
     print (item)
     leighton.main(item.split())
 
-
-
-if __name__ == '__main__':
-    start_time = time.time() 
+def help():
+    print ('python cores.py datafile')
+    
+def parse_command_line(argv):
     input_file  = 'cores.dat'
     if len(sys.argv[1:])>0:
         try:
@@ -45,24 +45,30 @@ if __name__ == '__main__':
             for opt, arg in opts: 
                 pass
             input_file = args[0]
-            m = mp.Manager()
-            memorizedPaths = m.dict()
-            filepaths = m.dict()
-            cutoff = 1 ##
-            # use all available CPUs
-            p = mp.Pool(initializer=init_worker, 
-                        initargs=(memorizedPaths,
-                                  filepaths,
-                                  cutoff))
-            print ('Opening {0}'.format(input_file))
-            with open(input_file) as f:
-                parameters_list = [x.strip() for x in  f.readlines() if len(x.strip())>0] 
-                for _ in p.imap_unordered(work, parameters_list, chunksize=1):
-                    pass
-                p.close()
-                p.join()
-                print('--- %s seconds ---' % (time.time() - start_time))            
         except getopt.GetoptError:
             help()
             sys.exit(2)
+    return input_file
+
+if __name__ == '__main__':
+    start_time = time.time() 
+    m = mp.Manager()
+    memorizedPaths = m.dict()
+    filepaths = m.dict()
+    cutoff = 1 ##
+    # use all available CPUs
+    p = mp.Pool(initializer=init_worker, 
+                initargs=(memorizedPaths,
+                          filepaths,
+                          cutoff))
+    input_file = parse_command_line(sys.argv[1:])
+    print ('Opening {0}'.format(input_file))
+    with open(input_file) as f:
+        parameters_list = [x.strip() for x in  f.readlines() if len(x.strip())>0] 
+        for _ in p.imap_unordered(work, parameters_list, chunksize=1):
+            pass
+        p.close()
+        p.join()
+        print('--- %s seconds ---' % (time.time() - start_time))            
+
    
