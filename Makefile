19c6c4678a55aea13a87cf98d92c31c16c1c771d
Simon Crase <simon@greenweaves.nz>
2018-01-19 15:42:32 +1300

    Started moving code to new file tree.cpp

--- Makefile
+++ Makefile
@@ -1,3 +1,23 @@
+CPPFLAGS=-g -pthread -I/sw/include/root 
+LDFLAGS=-g
+LDLIBS=-L/sw/lib/root -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint \
+       -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lz -L/sw/lib -lfreetype -lz \
+       -Wl,-framework,CoreServices -Wl,-framework,ApplicationServices -pthread -Wl,-rpath,/sw/lib/root \
+       -lm -ldl
+
 all : barnes_hut.exe
-barnes_hut.exe: barnes_hut.cpp tree.h
-	g++ -O3 -g -o barnes_hut barnes_hut.cpp
+
+SRCS=barnes_hut.cpp tree.cpp
+OBJS=$(subst .cpp,.o,$(SRCS))
+
+barnes_hut.exe: $(OBJS)
+	g++ $(LDFLAGS) -o barnes_hut.exe ${OBJS} 
+
+barnes_hut.o: barnes_hut.cpp tree.h
+	g++ $(CPPFLAGS) -c barnes_hut.cpp
+
+tree.o: tree.h tree.cpp
+	g++ $(CPPFLAGS) -c tree.cpp
+	
+#barnes_hut.exe: barnes_hut.cpp tree.h
+#	g++ -O3 -g -o barnes_hut barnes_hut.cpp
--- tree.h
+++ tree.h
@@ -106,24 +106,7 @@ public:
   
   // Compute the force of the present node onto another node, divided by
   // the other node's mass, and divided by the gravitational constant G.
-  void accelerationOn(Node const* other, double& fx, double& fy, double& fz,double dsqr) const {
-    // Introduce a cut-off distance to avoid numerical instability in case two
-    // nodes are too close to each other.
-    static const double cutoff_dist = 0.002;
-    static const double cutoff_dist_sqr = cutoff_dist*cutoff_dist;
-    if (dsqr < cutoff_dist_sqr)
-      fx = fy = fz= 0.;
-    else {
-      double mx, my, mz, o_x, o_y,o_z;
-      this->get_m_pos(mx, my, mz);
-      other->getPos(o_x, o_y, o_z);
-      const double m = this->getMass();
-      const double inv_d_cube = std::pow(dsqr, -3./2.); // The force goes like 1/r^2.
-      fx = (mx - o_x*m) * inv_d_cube;
-      fy = (my - o_y*m) * inv_d_cube;
-      fy = (mz - o_z*m) * inv_d_cube;
-    }
-  }
+  void accelerationOn(Node const* other, double& fx, double& fy, double& fz,double dsqr) const ;
 private:
   // Compute the index of the next sub-quadrant along a given direction
   // (there are two possibilities).
