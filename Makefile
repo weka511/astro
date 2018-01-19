54657b8de6fa4121f9997bae8330b9404e472a8b
Simon Crase <simon@greenweaves.nz>
2018-01-19 16:10:18 +1300

    Split into 3 source files + headers

--- Makefile
+++ Makefile
@@ -1,23 +1,22 @@
-CPPFLAGS=-g -pthread -I/sw/include/root 
-LDFLAGS=-g
-LDLIBS=-L/sw/lib/root -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint \
-       -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lz -L/sw/lib -lfreetype -lz \
-       -Wl,-framework,CoreServices -Wl,-framework,ApplicationServices -pthread -Wl,-rpath,/sw/lib/root \
-       -lm -ldl
-
-all : barnes_hut.exe
-
-SRCS=barnes_hut.cpp tree.cpp
+CPPFLAGS=-g -O3 -pthread -I/sw/include/root 
+LDFLAGS=-g -O3
+LDLIBS=
+CC=gcc
+CXX=g++
+RM=rm -f
+SRCS=barnes_hut.cpp galaxy.cpp tree.cpp
 OBJS=$(subst .cpp,.o,$(SRCS))
 
-barnes_hut.exe: $(OBJS)
-	g++ $(LDFLAGS) -o barnes_hut.exe ${OBJS} 
+all : galaxy.exe
 
-barnes_hut.o: barnes_hut.cpp tree.h
-	g++ $(CPPFLAGS) -c barnes_hut.cpp
+clean :
+	${RM} ${OBJS}
+	
+galaxy.exe: $(OBJS)
+	${CXX} $(LDFLAGS) -o galaxy.exe ${OBJS} ${LDLIBS}
+
+barnes_hut.o: barnes_hut.cpp tree.h barnes_hut.h
+
+galaxy.o: galaxy.cpp barnes_hut.h tree.h
 
 tree.o: tree.h tree.cpp
-	g++ $(CPPFLAGS) -c tree.cpp
-	
-#barnes_hut.exe: barnes_hut.cpp tree.h
-#	g++ -O3 -g -o barnes_hut barnes_hut.cpp
--- barnes_hut.cpp
+++ barnes_hut.cpp
@@ -37,7 +37,7 @@
 #include <cassert>
 
 #include "tree.h"
-
+#include "barnes_hut.h"
 
 // Barnes-Hut algorithm: Creation of the quad-tree. This function adds
 // a new body into a quad-tree node. Returns an updated version of the node.
@@ -144,74 +144,4 @@ void save_bodies( std::vector<Body*>& bodies, int i, std::string path){
     }
 }
 
-int main() {
-    // Theta-criterion of the Barnes-Hut algorithm.
-    const double theta = 0.5;
-    // Mass of a body.
-    const double mass = 1.0;
-    // Initially, the bodies are distributed inside a circle of radius ini_radius.
-    const double ini_radius = 0.1;
-    // Initial maximum velocity of the bodies.
-    const double inivel = 0.1;
-    // The "gravitational constant" is chosen so as to get a pleasant output.
-    const double G = 4.e-6;
-    // Discrete time step.
-    const double dt = 1.e-3;
-    // outside the initial radius are removed).
-    const int numbodies = 1000;
-    // Number of time-iterations executed by the program.
-    const int max_iter = 10000;
-    // Frequency at which PNG images are written.
-    const int img_iter = 20;
-
-    std::string path = ".\\configs\\";
-    
-    // The pseudo-random number generator is initialized at a deterministic
-    // value, for proper validation of the output for the exercise series.
-    std::srand(1);
-    // x- and y-pos are initialized to a square with side-length 2*ini_radius.
-    std::vector<double> posx(numbodies), posy(numbodies), posz(numbodies);
-    for (int i=0; i<numbodies; ++i) {
-        posx[i] = ((double) std::rand() / (double)RAND_MAX) * 2.*ini_radius + 0.5-ini_radius;
-        posy[i] = ((double) std::rand() / (double)RAND_MAX) * 2.*ini_radius + 0.5-ini_radius;
-	posz[i] = ((double) std::rand() / (double)RAND_MAX) * 2.*ini_radius + 0.5-ini_radius;
-    }
-    // Initially, the bodies have a radial velocity of an amplitude proportional to
-    // the distance from the center. This induces a rotational motion creating a
-    // "galaxy-like" impression.
-    std::vector<Body*> bodies;
-    for (int i=0; i<numbodies; ++i) {
-        const double px = posx[i];
-        const double py = posy[i];
-	const double pz = posz[i];
-        const double rpx = px-0.5;
-        const double rpy = py-0.5;
-        const double rnorm = std::sqrt(sqr(rpx)+sqr(rpy));
-        if ( rnorm < ini_radius ) {
-            const double vx = -rpy * inivel * rnorm / ini_radius;
-            const double vy =  rpx * inivel * rnorm / ini_radius;
-	    const double vz = 0;
-            bodies.push_back( new Body(mass, px, py, pz, vx, vy,vz) );
-        }
-    }
-
-    // Principal loop over time iterations.
-    for (int iter=0; iter<max_iter; ++iter) {
-        // The quad-tree is recomputed at each iteration.
-        Node* root = 0;
-        for (unsigned i=0; i<bodies.size(); ++i) {
-            bodies[i] -> resetToZerothQuadrant();
-            root = add(bodies[i], root);
-        }
-        // Computation of forces, and advancement of bodies.
-        verlet(bodies, root, theta, G, dt);
-        // De-allocate the quad-tree.
-        delete root;
-        // Output.
-        if (iter%img_iter==0) {
-            std::cout << "Writing images at iteration " << iter << std::endl;
-            save_bodies(bodies, iter/img_iter,path);
-        }
-    }
-}
 
