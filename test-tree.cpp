c49c8b5e0913e3883e7e4a09dcb90329f98a79a2
Simon Crase <simon@greenweaves.nz>
2018-01-20 14:29:21 +1300

    Split tests out into separate files

---
+++ test-tree.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+
+#include "tree.h"
+#include "catch.hpp"
+
+TEST_CASE( "Foo", "[foo]" ) {
+    REQUIRE( 1 == 0 );
+ 
+}
