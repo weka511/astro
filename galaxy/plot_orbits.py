abc6611c28ad45583cbbc7e8dce53ed21f07147f
Simon Crase <simon@greenweaves.nz>
2018-01-23 08:59:53 +1300

    Started on plotting orbits

---
+++ galaxy/plot_orbits.py
@@ -0,0 +1,47 @@
+'''
+ Copyright (C) 2017 Greenweaves Software Pty Ltd
+
+ This is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This software is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this software.  If not, see <http://www.gnu.org/licenses/>
+
+  Companion to barnes_hut.cpp - plot images
+'''
+import os, re, sys, numpy as np, matplotlib.pyplot as plt,mpl_toolkits.mplot3d
+
+colours=['r','g','b','m','c','y']
+def plot(data):
+    def get_coordinates(body,i):
+        return [d[body][i] for d in data]
+    ax = plt.gcf().add_subplot(111, aspect='equal', projection='3d')
+    for body in range(len(data[0])):
+        ax.scatter(get_coordinates(body,0)[0],get_coordinates(body,1)[0],get_coordinates(body,2)[0],c=colours[body%len(colours)])
+        ax.scatter(get_coordinates(body,0),get_coordinates(body,1),get_coordinates(body,2),edgecolor=colours[body%len(colours)],s=1) 
+
+def extract(config_path = './configs/',rows=[0,1,2,55,100,400]):
+    result=[]
+    i=0
+    for file_name in os.listdir(config_path):
+        m = re.search('body_[0-9]+.dat',file_name)
+        if m:
+            positions = np.loadtxt(os.path.join(config_path,m.group(0)))
+            result.append([positions[i] for i in rows])
+            i+=1
+            if i>25:return result
+    return result
+
+if __name__=='__main__':
+    data=extract()
+    plot(data)
+    plt.show()
+    
+        
