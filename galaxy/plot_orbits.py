034d2fb7349f4e07749fbc6c7a806ae003f4165d
Simon Crase <simon@greenweaves.nz>
2018-01-23 09:21:06 +1300

    Select bodies for plotting at random

--- galaxy/plot_orbits.py
+++ galaxy/plot_orbits.py
@@ -16,32 +16,35 @@
 
   Companion to barnes_hut.cpp - plot images
 '''
-import os, re, sys, numpy as np, matplotlib.pyplot as plt,mpl_toolkits.mplot3d
+import os, re, sys, numpy as np, matplotlib.pyplot as plt,mpl_toolkits.mplot3d,random
 
 colours=['r','g','b','m','c','y']
-def plot(data):
+def plot(data,rows=[]):
     def get_coordinates(body,i):
         return [d[body][i] for d in data]
     ax = plt.gcf().add_subplot(111, aspect='equal', projection='3d')
     for body in range(len(data[0])):
-        ax.scatter(get_coordinates(body,0)[0],get_coordinates(body,1)[0],get_coordinates(body,2)[0],c=colours[body%len(colours)])
+        ax.scatter(get_coordinates(body,0)[0],get_coordinates(body,1)[0],get_coordinates(body,2)[0],c=colours[body%len(colours)],
+                   label="Body: {0}".format(rows[body]))
         ax.scatter(get_coordinates(body,0),get_coordinates(body,1),get_coordinates(body,2),edgecolor=colours[body%len(colours)],s=1) 
+        plt.legend(loc='best')
+
 
 def extract(config_path = './configs/',rows=[0,1,2,55,100,400]):
     result=[]
-    i=0
     for file_name in os.listdir(config_path):
-        m = re.search('body_[0-9]+.dat',file_name)
+        m = re.search('body_[0-9]+00.dat',file_name)
         if m:
             positions = np.loadtxt(os.path.join(config_path,m.group(0)))
             result.append([positions[i] for i in rows])
-            i+=1
-            if i>25:return result
     return result
 
 if __name__=='__main__':
-    data=extract()
-    plot(data)
+    M=6
+    N=1000
+    rows=random.sample(range(N),M) 
+    data=extract(rows=rows)
+    plot(data,rows=rows)
     plt.show()
     
         
