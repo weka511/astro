da237ba4fa2c890253da8bb33917f9988ff5e391
Simon Crase <simon@greenweaves.nz>
2018-01-24 08:28:09 +1300

    Fixed dates in copyright notices

--- galaxy/make_img.py
+++ galaxy/make_img.py
@@ -1,5 +1,5 @@
 '''
- Copyright (C) 2017 Greenweaves Software Pty Ltd
+ Copyright (C) 2017-2018 Greenweaves Software Pty Ltd
 
  This is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
