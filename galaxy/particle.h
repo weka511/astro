d2d32f6414ed92923d090a370de1fdc97e4f930c
Simon Crase <simon@greenweaves.nz>
2018-01-30 10:08:39 +1300

    Documented particles

--- galaxy/particle.h
+++ galaxy/particle.h
@@ -20,8 +20,16 @@
 #include <vector>
 #include <cmath>
 
+/**
+ * A Particle represents one of the bodies whose motion is being simulated.
+ * It is just a passive container for data
+ */
 class Particle {
   public:
+    /**
+     * Create particle and initialize position, mass, and velocity. 
+	 * Force calculation of acceleration by setting it to NAN.
+     */
 	Particle(double x,	double y, double z, 
 		double vx, 	double vy, double vz,
 		double m)
@@ -30,39 +38,70 @@ class Particle {
 		_ax(NAN), _ay(NAN), _az(NAN)
 	{}
 	
-	Particle (double x, double vx,double m=1)
-	: Particle(x,0,0,vx,0,0,m) {}
-	
 	virtual ~Particle() {
 	}
 
+	/**
+	 *  Accessor for current position
+	 */
 	void getPos(double& x,	double& y, double& z) {x=_x;y=_y;z=_z;}
 	
+	/**
+	 *  Set current position
+	 */
 	void setPos(double x,	double y, double z) {_x=x;_y=y;_z=z;}
 	
+	/**
+	 *  Accessor for current velocity
+	 */
 	void getVel(double& vx,	double& vy, double& vz) {vx=_vx;vy=_vy;vz=_vz;}
 	
+	/**
+	 *  Set current velocity
+	 */
 	void setVel(double vx,	double vy, double vz) {_vx=vx;_vy=vy;_vz=vz;}
 	
+	/**
+	 *  Accessor for current acceleration
+	 */
 	void getAcc(double& ax,	double& ay, double& az) {ax=_ax;ay=_ay;az=_az;}
 	
+	/**
+	 *  Set current acceleration
+	 */
 	void setAcc(double ax,	double ay, double az) {_ax=ax;_ay=ay;_az=az;}
 	
+	/**
+	 *  Accessor for mass
+	 */
 	double getMass() {return _m;}
 	
   private:
+	/**
+	 * Position
+	 */
 	double _x;
 	double _y;
 	double _z;
+	
+	/**
+	 * Velocity
+	 */
 	double _vx;
 	double _vy;
 	double _vz;
+	
+	/**
+	 * Mass
+	 */
 	double _m;
+	
+	/**
+	 * Acceleration
+	 */
 	double _ax;
 	double _ay;
 	double _az;
 };
-
-
 
 #endif
