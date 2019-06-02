6fac524ed06b90d3d9c52072da50bcd7d0b618bb
Simon Crase <simon@greenweaves.nz>
2019-06-03 09:03:17 +1200

    Issue #1: reformatted output

--- 1.5.py
+++ 1.5.py
@@ -13,19 +13,26 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-def identify_commensurables(periods,tolerance=0.001):
+def identify_commensurabilities(periods,tolerance=0.001):
      ratios = sorted([(i,j,periods[i]/periods[j]) for j in range(len(periods)) for i in range(j)],
                      key=lambda x:x[2])
-     print (ratios)
+
      for p in range(1,11):
+          commensurability = p/(p+1)
           for i,j,ratio in ratios:
-               if abs(ratio-p/(p+1))<tolerance:
-                    print (i,j,ratio,p/(p+1))
+               if abs(ratio-commensurability)<tolerance:
+                    print ('i={0},j={1},ratio={2},commensurability={3}:{4}({5}) [{6}]'.format(i,
+                                                                                        j,
+                                                                                        ratio,
+                                                                                        p,
+                                                                                        p+1,
+                                                                                        commensurability,
+                                                                                        abs(commensurability-ratio)))
     
 if __name__=='__main__':
      with open('data/1.5.txt') as data:
           periods = []
           for line in data:
                periods.append(float(line.strip()))
-          identify_commensurables(periods)
+          identify_commensurabilities(periods)
             
