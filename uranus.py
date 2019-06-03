4e22d43c578d090f4e745c1de5760a0d197ebd0c
Simon Crase <simon@greenweaves.nz>
2019-06-04 09:58:50 +1200

    Parse command line arguments

--- uranus.py
+++ uranus.py
@@ -24,15 +24,20 @@ def generate_orbit(T1=1.413, L=1.546, U=2.101, range_i=range(1,3)):
     return Ts
 
 if __name__=='__main__':
-    N      = 1000000
-    random.seed()
+    import argparse
+    parser = argparse.ArgumentParser(description='Calculate proabbility of delta_n<0.1 for problem 1.3.')    
+    parser.add_argument('-N',type=int, help='Number of sets of orbital periods',default=100000)
+    parser.add_argument('--seed','-s',help='Seed for random number generator',default=None)
+    args = parser.parse_args()
+    random.seed(args.seed)
     delta_n = []
-    for orbits in  [generate_orbit() for i in range(N)]:
+    for orbits in  [generate_orbit() for i in range(args.N)]:
         n = [360/T for T in orbits]
         delta_n.append(abs(n[0]-3*n[1] + 2*n[2]))
     bins=10**(np.arange(-2,4,dtype=float))
     plt.xscale('log')
     plt.hist(delta_n,bins=bins)
-    plt.title('Probability: {0}%'.format(100*len([delta for delta in delta_n if delta<0.1])/100000))
+    plt.title('Probability: {0:.2f}% after {1:,} iterations'.format(100*len([delta for delta in delta_n if delta<0.1])/100000,
+                                         args.N))
     plt.show()
             
