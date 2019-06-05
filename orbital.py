2265d37bdb87aebfda0069c1d8d901fbdd3015d5
Simon Crase <simon@greenweaves.nz>
2019-06-05 13:34:32 +1200

    Calculate eccentric anomaly

--- orbital.py
+++ orbital.py
@@ -13,46 +13,59 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-import numpy as np
-from math import cos,sin,radians,isclose
+from numpy import matmul,sign
+from math import cos,sin,radians,isclose,degrees,pi
 
-def compose(omega,I,Omega):
+def compose(omega=0,I=0,Omega=0):
     cos_omega = cos(radians(omega))
     sin_omega = sin(radians(omega))
-    assert isclose(cos_omega*cos_omega + sin_omega*sin_omega,1,abs_tol=1e-10)
+   
     P1        = [[cos_omega, -sin_omega, 0],\
                  [sin_omega,  cos_omega, 0],\
                  [0,          0,         1]]
-    assert isclose(np.linalg.det(P1),1,abs_tol=1e-10)
+    
     cos_I     = cos(radians(I))
     sin_I     = sin(radians(I))
-    assert isclose(cos_I*cos_I + sin_I*sin_I,1,abs_tol=1e-10)
+   
     P2        = [[1,          0,      0],     \
                  [0,          cos_I, -sin_I], \
                  [0,          sin_I,  cos_I]]
-    assert isclose(np.linalg.det(P2),1,abs_tol=1e-10)
+    
     cos_Omega = cos(radians(Omega))
     sin_Omega = sin(radians(Omega))
-    assert isclose(cos_Omega*cos_Omega + sin_Omega*sin_Omega,1,abs_tol=1e-10)
+    
     P3        =  [[cos_Omega, -sin_Omega, 0],\
                   [sin_Omega,  cos_Omega, 0],\
                   [0,          0,         1]]
-    assert isclose(np.linalg.det(P3),1,abs_tol=1e-10)
-    Pj = np.matmul(P3, np.matmul(P2, P1))
-    assert isclose(np.linalg.det(Pj),1,abs_tol=1e-10)
-    return Pj
+   
+    return matmul(P3, matmul(P2, P1))
+
+
+def kepler(eccentricy=0,mean_anomaly=0,tolerance=0.1e-7,N=10000,k=0.85):
+    M = mean_anomaly % (2 * pi)
+    
+    E = M + sign(sin(M)*k*eccentricy)
+    for i in range(N):
+        correction = (E - eccentricy*sin(E) - M)/(1-eccentricy*cos(E))
+        if abs(correction)<tolerance: return E
+        E -= correction
     
 if __name__=='__main__':
     import unittest
     
-    class Test1(unittest.TestCase):
-        def test1(self):
-            a = [[1, 1],\
-                 [0, 1]]
-            b = [[4, 1], \
-                 [2, 2]]
-            print (np.matmul(a, b))            
-            self.assertEqual(1,1)
-            print(compose(14.7392,1.30537,100.535))
-
+    class TestJupiter(unittest.TestCase):
+        def test_mult(self):
+            print(compose(omega=14.7392,I=1.30537,Omega=100.535))
+            
+    class TestKepler(unittest.TestCase):
+        def test_kepler(self):
+            eccentric_anomaly = kepler(eccentricy=0.205635,mean_anomaly=1.2)
+            M = eccentric_anomaly - 0.205635 * sin(eccentric_anomaly)
+            self.assertAlmostEqual(1.2,M)
+        def test_kepler_jupiter(self):
+            self.assertAlmostEqual(radians(189.059),
+                                   kepler(mean_anomaly=radians(189.495),
+                                          eccentricy=0.0484007),
+                                   places=4)
+            
     unittest.main()
