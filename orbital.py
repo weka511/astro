6dd45dd022cdd55b1af1a2ba9408481e66dffbf5
Simon Crase <simon@greenweaves.nz>
2019-06-07 13:16:13 +1200

    Orbital calcs now pass tests from MD

--- orbital.py
+++ orbital.py
@@ -50,16 +50,45 @@ def get_XYZ(omega=0,I=0,Omega=0,f=0,r=1):
     return (r * (cos_Omega*cos_omega_f - sin_Omega*sin_omega_f*cos_I),
             r * (sin_Omega*cos_omega_f + cos_Omega*sin_omega_f*cos_I),
             r * sin_omega_f*sin_I)
-    
-def kepler(eccentricy=0,mean_anomaly=0,tolerance=0.1e-12,N=10000,k=0.85):
+
+def get_lambda(T=0,lambda0=34.40438,lambda_dot=557078.35,Nr=8):
+    lambdaT = lambda0 + (lambda_dot/3600 + 360 * Nr) *T
+    if lambdaT< 0:
+        while lambdaT<0:
+            lambdaT+=360
+    else:
+        while lambdaT>360:
+            lambdaT-=360
+    return lambdaT
+
+def get_eccentricity(T=0,e0=0.04839266,e_dot=-12280/1e8):
+    return e0 + e_dot * T
+
+def get_semimajor_axis(T=0,a0=5.20336301,a_dot=60377/1e8):
+    return a0 + a_dot * T
+
+def get_varpi(T=0,varpi0=14.75385,varpi_dot=839.93):
+    return varpi0+varpi_dot * T / 3600
+
+# get_xy
+#
+# Compute 2D position using 2.41
+
+def get_xy(T=0,lambdaT=0,eccentricity=0.0484007,a=5.20332,varpi = 14.7392):
+    M = lambdaT - varpi
+    E = kepler(eccentricity=eccentricity,mean_anomaly=radians(M))
+    return a*(cos(E)-eccentricity),a*sqrt(1-eccentricity*eccentricity)*sin(E)
+
+def kepler(eccentricity=0,mean_anomaly=0,tolerance=0.1e-12,N=10000,k=0.85):
     M = mean_anomaly % (2 * pi)
-    E = M + sign(sin(M)*k*eccentricy)
+    E = M + sign(sin(M)*k*eccentricity)
     
     for i in range(N):
-        correction = (E - eccentricy*sin(E) - M)/(1 - eccentricy * cos(E))
-        if abs(correction)<tolerance: return E
+        correction = (E - eccentricity*sin(E) - M)/(1 - eccentricity * cos(E))
+        if abs(correction)<tolerance*(M+E)*0.5: return E
         E -= correction
-    
+
+
 if __name__=='__main__':
     import unittest
     
@@ -92,16 +121,50 @@ if __name__=='__main__':
             self.assertAlmostEqual(-5.00336, X, places=5)
             self.assertAlmostEqual(-2.16249, Y, places=5)
             self.assertAlmostEqual(0.121099, Z, places=6)
+
+        def test_get_T(self):
+            self.assertAlmostEqual(-0.06266423,(2449256.189-2451545.0)/36525) # check Julian centuries
             
+        def test_get_lambda(self):
+            self.assertAlmostEqual(204.234,get_lambda(T=-0.06266423),places=3)
+            
+        def test_get_xy(self):
+            x,y = get_xy(T          = -0.06266423,
+                         lambdaT    = get_lambda(T=-0.06266423),
+                         eccentricity = get_eccentricity(T=-0.06266423),
+                         a          = get_semimajor_axis(-0.06266423),
+                         varpi      = get_varpi(-0.06266423))
+            self.assertAlmostEqual(-5.39027,x,places=5)
+            self.assertAlmostEqual(-0.818277,y,places=5)
+            
+        def test_eccentricty(self):
+            self.assertAlmostEqual(0.0484007,get_eccentricity(T=-0.06266423),places=6)
+            
+        def test_semimajor_axis(self):
+            self.assertAlmostEqual(5.20332,get_semimajor_axis(T=-0.06266423),places=4)
+            
+        def test_varpi(self):
+            self.assertAlmostEqual(14.7392,get_varpi(T=-0.06266423),places=4)        
+               
     class TestKepler(unittest.TestCase):
+        # test_kepler_inverse
+        #
+        # Verify solution to Kepler's equation for a range of parameters
         def test_kepler_inverse(self):
-            eccentric_anomaly = kepler(eccentricy=0.205635,mean_anomaly=1.2)
-            M = eccentric_anomaly - 0.205635 * sin(eccentric_anomaly)
-            self.assertAlmostEqual(1.2,M)
+            N = 25
+            for i in range(N):
+                mean_anomaly = 2 * pi /N
+                eccentric_anomaly = kepler(eccentricity=0.205635,mean_anomaly=mean_anomaly)
+                M                 = eccentric_anomaly - 0.205635 * sin(eccentric_anomaly)
+                self.assertAlmostEqual(mean_anomaly,M)
+            
+        # test_kepler_jupiter
+        #
+        # See paragraph before MD 2.123
         def test_kepler_jupiter(self):
             self.assertAlmostEqual(radians(189.059),
                                    kepler(mean_anomaly=radians(189.495),
-                                          eccentricy=0.0484007),
+                                          eccentricity=0.0484007),
                                    places=4)
             
     unittest.main()
