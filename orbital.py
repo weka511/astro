1d493099bd4e0cf3c1c6cefabf03f08cc4876b21
Simon Crase <simon@greenweaves.nz>
2026-09-09 08:13:38 +1200

    #40 Started converting to current standard

--- orbital.py
+++ orbital.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2019 Greenweaves Software Limited
+# Copyright (C) 2019-2026 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,60 +15,61 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-from numpy import matmul,sign,arctan2,clip
-from math import cos,sin,radians,isclose,degrees,pi,sqrt,floor,modf
+import numpy as np
+from math import radians,isclose,degrees,floor,modf
 
-# compose
-#
-# Rotate coordinates in 3D: MS 2.119, 2.120, & 2.121
-#
-# Parameters:
-#    omega  Argument of pericentre
-#    I      Inclination       
-#    Omega  Longitude of ascending node
 
 def compose(omega=0,I=0,Omega=0):
-    cos_omega = cos(omega)
-    sin_omega = sin(omega)
-    cos_I     = cos(I)
-    sin_I     = sin(I)
-    cos_Omega = cos(Omega)
-    sin_Omega = sin(Omega)
-    
-    P1        = [[cos_omega, -sin_omega, 0],\
-                 [sin_omega,  cos_omega, 0],\
-                 [0,          0,         1]]
-   
-    P2        = [[1,          0,      0],     \
-                 [0,          cos_I, -sin_I], \
-                 [0,          sin_I,  cos_I]]
-    
-    P3        =  [[cos_Omega, -sin_Omega, 0],\
-                  [sin_Omega,  cos_Omega, 0],\
-                  [0,          0,         1]]
-   
-    return matmul(P3, matmul(P2, P1))
+    '''
+     Rotate coordinates in 3D: MS 2.119, 2.120, & 2.121
 
-# get_XYZ
-#
-# Calculate position of particle in 3D: MD 2.122
-#
-# Parameters:
-#    omega  Argument of pericentre
-#    I      Inclination       
-#    Omega  Longitude of ascending node
-#    f      True anomaly
-#    r      radius
+    Parameters:
+        omega  Argument of pericentre
+        I      Inclination       
+        Omega  Longitude of ascending node
+    '''
+    cos_omega = np.cos(omega)
+    sin_omega = np.sin(omega)
+    cos_I = np.cos(I)
+    sin_I = np.sin(I)
+    cos_Omega = np.cos(Omega)
+    sin_Omega = np.sin(Omega)
+    
+    P1 = np.array([[cos_omega, -sin_omega, 0],
+                   [sin_omega,  cos_omega, 0],
+                   [0,          0,         1]])
+   
+    P2 = np.array([[1, 0,      0],
+                   [0, cos_I, -sin_I], 
+                   [0, sin_I,  cos_I]])
+    
+    P3 = np.array([[cos_Omega, -sin_Omega, 0],
+                   [sin_Omega,  cos_Omega, 0],
+                   [0,          0,         1]])
+   
+    return np.matmul(P3, np.matmul(P2, P1))
+
+
 def get_XYZ(omega=0,I=0,Omega=0,f=0,r=1):
-    cos_Omega   = cos(Omega)
-    sin_Omega   = sin(Omega)
-    cos_I       = cos(I)
-    sin_I       = sin(I)
-    cos_omega_f = cos(omega + f)
-    sin_omega_f = sin(omega + f)
-    return (r * (cos_Omega*cos_omega_f - sin_Omega*sin_omega_f*cos_I),
+    '''
+    Calculate position of particle in 3D: MD 2.122
+
+    Parameters:
+        omega  Argument of pericentre
+        I      Inclination       
+        Omega  Longitude of ascending node
+        f      True anomaly
+        r      radius
+    '''
+    cos_Omega = np.cos(Omega)
+    sin_Omega = np.sin(Omega)
+    cos_I = np.cos(I)
+    sin_I = np.sin(I)
+    cos_omega_f = np.cos(omega + f)
+    sin_omega_f = np.sin(omega + f)
+    return np.array([r * (cos_Omega*cos_omega_f - sin_Omega*sin_omega_f*cos_I),
             r * (sin_Omega*cos_omega_f + cos_Omega*sin_omega_f*cos_I),
-            r * sin_omega_f*sin_I)
+            r * sin_omega_f*sin_I])
 
 # get_lambda
 #
@@ -132,11 +133,11 @@ def get_longitude_of_perihelion(T=0,varpi0=14.75385,varpi_dot=839.93):
 #                       if correction still exceeds tolerance after N iterations
 
 def get_true_anomaly(E,e,N=1000,tolerance=1e-12):
-    cos_f = (cos(E)-e)/(1-e*cos(E))
+    cos_f = (np.cos(E)-e)/(1-e*np.cos(E))
     f     = E  # E and F should be in same quadrant, so use E as starting value
     for i in range(N):
         if abs(f) < tolerance: return f
-        correction = (cos(f)-cos_f)/sin(f)
+        correction = (np.cos(f)-cos_f)/np.sin(f)
         f += correction
         if (abs(correction)<tolerance):
             return f
@@ -148,16 +149,16 @@ def get_true_anomaly(E,e,N=1000,tolerance=1e-12):
 # Compute 2D position using 2.41
 #
 #    Parameters:
-#        T               Number of Julian centuries since start of epoch
+#        T               Number of Julian centuries np.since start of epoch
 #        lambdaT         Mean longitude in radians
 #        e               eccentricity
 #        a               Semi-major axis
-#        varpi           longitude of perihelion in radians
+#        var           longitude of perihelion in radians
 
 def get_xy(T=0,lambdaT=0,e=0.0484007,a=5.20332,varpi = 14.7392):
     M = lambdaT - varpi #MD: paragraph before 2.123
     E = get_eccentric_anomaly(e=e,M=M)
-    return a * (cos(E) - e),a * sqrt(1 - e*e)*sin(E)
+    return a * (np.cos(E) - e),a * np.sqrt(1 - e*e)*np.sin(E)
 
 # get_eccentric_anomaly
 #
@@ -172,10 +173,10 @@ def get_xy(T=0,lambdaT=0,e=0.0484007,a=5.20332,varpi = 14.7392):
 #             k             Paramter used in Danby's starting value - MD 2.64
 def get_eccentric_anomaly(e=0,M=0,rel_tol=1e-7, abs_tol=1e-9,N=10000,k=0.85):
    
-    E = M + sign(sin(M) * k * e)  # Danby's starting value - MD 2.64
+    E = M + np.sign(np.sin(M) * k * e)  # Danby's starting value - MD 2.64
     
     for i in range(N):
-        correction = (E - e * sin(E) - M)/(1 - e * cos(E))
+        correction = (E - e * np.sin(E) - M)/(1 - e * np.cos(E))
         if isclose(correction,0,rel_tol=rel_tol,abs_tol=abs_tol): return E
         E -= correction
         
@@ -236,7 +237,7 @@ def create_orbit(planet,
                        a       = a,
                        varpi   = radians(varpi))
 
-        W   = matmul(Rotation,[[x],[y],[0]])
+        W   = np.matmul(Rotation,[[x],[y],[0]])
         Xs.append(W[0][0])
         Ys.append(W[1][0])
         Zs.append(W[2][0])
@@ -263,7 +264,7 @@ def is_minimum(a,b,c):
 # Get Euclidean distance between two points
 
 def get_distance(x0,y0,z0,x1,y1,z1):
-    return sqrt((x0-x1)*(x0-x1) + (y0-y1)*(y0-y1) + (z0-z1)*(z0-z1))
+    return np.sqrt((x0-x1)*(x0-x1) + (y0-y1)*(y0-y1) + (z0-z1)*(z0-z1))
 
 # get_julian_date
 #
@@ -355,8 +356,8 @@ if __name__=='__main__':
         def test_get_XYZ(self):
             x     = -5.39027
             y     = -0.818277
-            r     = sqrt(x*x + y * y)
-            f     = arctan2(y,x)
+            r     = np.sqrt(x*x + y * y)
+            f     = np.arctan2(y,x)
             X,Y,Z = get_XYZ(omega=radians(14.7392 -100.535 ),I=radians(1.30537),Omega=radians(100.535),r=r,f=f)
             self.assertAlmostEqual(-5.00336, X, places=5)
             self.assertAlmostEqual(-2.16249, Y, places=5)
@@ -366,7 +367,7 @@ if __name__=='__main__':
             self.assertAlmostEqual(-0.06266423,(2449256.189-2451545.0)/36525) # check Julian centuries
             
         def test_get_mean_longitude(self):
-            self.assertAlmostEqual(radians(204.234),get_mean_longitude(T=-0.06266423)%(2*pi),places=3)
+            self.assertAlmostEqual(radians(204.234),get_mean_longitude(T=-0.06266423)%(2*np.pi),places=3)
             
         def test_get_xy(self):
             x,y = get_xy(T            = -0.06266423,
@@ -397,9 +398,9 @@ if __name__=='__main__':
         def test_kepler_inverse(self):
             N = 25
             for i in range(N):
-                mean_anomaly = 2 * pi /N
+                mean_anomaly = 2 * np.pi /N
                 E            = get_eccentric_anomaly(e=0.205635,M=mean_anomaly)
-                M            = E - 0.205635 * sin(E)
+                M            = E - 0.205635 * np.sin(E)
                 self.assertAlmostEqual(mean_anomaly,M)
             
         # test_kepler_jupiter
