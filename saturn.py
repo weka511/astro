7a616a7ef4d00bc28bc779649d16696b4a95c0ba
Simon Crase <simon@greenweaves.nz>
2026-09-08 17:27:38 +1200

    #35 Now finding the 4 orbits pairs

--- saturn.py
+++ saturn.py
@@ -26,75 +26,106 @@ from matplotlib.pyplot import figure, show
 import numpy as np
 
 
-def get_bounds(n_ratio, max_p=100):
+def get_bounds(n_ratio):
      '''
      calculate p and p' from Murray and Dermott, Section 1.7.
      We use r0 for the lower bound (MD p'/(p'+1)) and r1 for the upper
-     '''
-     if 1/3 < n_ratio and n_ratio < 1/2:  return (1/3, 1/2)
-     
+     '''  
      r0 = 0
-     r1 = None
-     for p in range(max_p):
-          r = p/(p + 1)
-          if r < n_ratio:
-               r0 = r
-          if r > n_ratio:
-               r1 = r
-               return (r0, r1)
+     r1 = 0
+     p = 1
 
+     while r1 < n_ratio:
+          r0 = r1
+          r1 = max(r1,p/(p + 1))
+          p += 1
+          
+     if 1/3 < n_ratio and n_ratio < 1/2: 
+          r0 = 1/3
+     return (r0, r1)
 
-def get_mean_motion_ratios(data, max_p=100):
-     rcs = []
-     for j in range(len(data)):
-          _, T1 = data[j]
+def generate_pairs(data):
+     for i in range(len(data)):
+          name_i, T1 = data[i]
           n1 = 360/T1
-          for i in range(j):
-               _, T2 = data[i]
+          for j in range(len(data)):
+               name_j, T2 = data[j]
                n2 = 360/T2
-               (r0, r1) = get_bounds(n1/n2, max_p=max_p)
-               a = (n1/n2 - r0)/(r1 - r0)   # Murray & Dermott (1.19)
-               b = 0 if a <= 0.5 else 1   # Murray & Dermott (1.20)
-               c = 2*np.pi*(a - b)    # Murray & Dermott (1.21)
-               rcs.append(((r0, r1), c))
-     return rcs
+               if n1 < n2:
+                    yield name_i,name_j,n1,n2
+                    
+def create_mean_motion_ratios(data):
+     product = []
+     for name_i,name_j,n1,n2 in generate_pairs(data):
+          (r0, r1) = get_bounds(n1/n2)
+          a = (n1/n2 - r0)/(r1 - r0)   # Murray & Dermott (1.19)
+          b = 0 if a <= 0.5 else 1   # Murray & Dermott (1.20)
+          c = 2*np.pi*(a - b)    # Murray & Dermott (1.21)
+          product.append((name_i,name_j,r0, r1, c))
+    
+     return product
 
 
 def parse_args():
      parser = ArgumentParser(description='Calculate probability of delta_n<0.1 for problem 1.4.')
      parser.add_argument('--data', default='./data', help=f'Path to data files')
      parser.add_argument('--figs', default='./figs', help=f'Path to plots')
+     parser.add_argument('--tolerance',default=0.15,type=float)
      return parser.parse_args()
 
-def create_data(data_path):
+def create_data(data_path,
+                exclude=[
+                     'Epimetheus',
+                     'Telesto',
+                     'Calypso',
+                     'Helene']):
      '''
      Read data file
      
      Parameters:
          data_path
+         exclude
      '''
      with open(data_path) as data_file:
           product = []
           data_reader = reader(data_file)
           for row in data_reader:
-               product.append([row[0],abs(float(row[1]))])     
+               if row[0] not in exclude:
+                    product.append([row[0],abs(float(row[1]))])     
      return product
+
+def get_bar(mean_motion_ratios,tolerance=0.15):
+     c_indices = np.argsort([abs(c) for _,_,_,_,c in mean_motion_ratios])
+     labels = []
+     cc = []
+     for i in c_indices:
+          name_i,name_j,r0, r1, c = mean_motion_ratios[i]
+          if r0 > 0 and abs(c) < tolerance:
+               labels.append(f'{name_i}-{name_j}')
+               cc.append(abs(c))
+               print (mean_motion_ratios[i])
+     return labels,cc,['xkcd:red','xkcd:green','xkcd:blue','xkcd:yellow']
 
 def main():
      args = parse_args()
 
-     rcs = get_mean_motion_ratios(
+     mean_motion_ratios = create_mean_motion_ratios(
                create_data(
                     (Path(args.data)/Path(__file__).stem).with_suffix('.csv')))
-     cs = sorted([abs(c) for _, c in rcs])
-     for i in range(len(cs)):
-          print(i, cs[i])
-     fig = figure(figsize=(6, 6))
-     ax = fig.add_subplot(1, 1, 1)
-     ax.hist(cs, bins=100,density=True)
-     ax.set_title('Distribution of c')
-     ax.set_xlabel('c')
-     print(len(set([r for r, _ in rcs])))
+
+     labels,cc,bar_colours = get_bar(mean_motion_ratios)
+ 
+     
+     cs = sorted([abs(c) for _,_,_,_,c in mean_motion_ratios])
+     fig = figure(figsize=(12, 12))
+     ax1 = fig.add_subplot(2, 1, 1)
+     ax1.hist(cs,bins=100)
+     ax1.set_title('Distribution of c')
+     ax1.set_xlabel('c')
+     ax2 = fig.add_subplot(2, 1, 2)
+     #cc = [c for c in cs if c <args.tolerance]
+     ax2.bar(labels,cc,color=bar_colours)
+   
      fig.savefig(Path(args.figs)/Path(__file__).stem)
 
      show()
