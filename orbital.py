081da504bbfd158ea33c1d666831783316a09ce7
Simon Crase <simon@greenweaves.nz>
2026-09-09 08:22:50 +1200

    #40 Replaced most math. with np.

--- orbital.py
+++ orbital.py
@@ -15,9 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
+'''
+Orbital calculations to support earth_mars.py
+'''
+from math import isclose,floor,modf
+from unittest import TestCase,main
 import numpy as np
-from math import radians,isclose,degrees,floor,modf
-
 
 def compose(omega=0,I=0,Omega=0):
     '''
@@ -77,14 +80,14 @@ def get_XYZ(omega=0,I=0,Omega=0,f=0,r=1):
 #
 # Parameters:
 #       T          Number of Julian centuries since start of epoch
-#       lambda0    mean longitude at T = 0 in degrees
+#       lambda0    mean longitude at T = 0 in np.degrees
 #       lambda_dot Rate of change of mean longitude in arc seconds per century 
 #       Nr
 #
 # Returns: mean longitude in radians
 
 def get_mean_longitude(T=0,lambda0=34.40438,lambda_dot=557078.35,Nr=8):
-    return radians(lambda0 + (lambda_dot/3600 + 360 * Nr) *T)
+    return np.radians(lambda0 + (lambda_dot/3600 + 360 * Nr) *T)
 
 # get_eccentricity
 #
@@ -120,7 +123,7 @@ def get_semimajor_axis(T=0,a0=5.20336301,a_dot=60377/1e8):
 #     varpi_dot
 
 def get_longitude_of_perihelion(T=0,varpi0=14.75385,varpi_dot=839.93):
-    return radians(varpi0 + varpi_dot * T / 3600)
+    return np.radians(varpi0 + varpi_dot * T / 3600)
 
 # get_true_anomaly
 #
@@ -223,9 +226,9 @@ def create_orbit(planet,
     Ys                        = []
     Zs                        = []
     ts                        = []
-    Rotation                  = compose(omega = radians(varpi - Omega), #MD 2.118
-                                         I     = radians(I),
-                                         Omega = radians(Omega))
+    Rotation                  = compose(omega = np.radians(varpi - Omega), #MD 2.118
+                                         I     = np.radians(I),
+                                         Omega = np.radians(Omega))
 
     for t,T in Times(From=From,To=To,Incr=Incr):
         x,y = get_xy(T       = T, 
@@ -235,7 +238,7 @@ def create_orbit(planet,
                                                     Nr         = Nr), 
                        e       = e,
                        a       = a,
-                       varpi   = radians(varpi))
+                       varpi   = np.radians(varpi))
 
         W   = np.matmul(Rotation,[[x],[y],[0]])
         Xs.append(W[0][0])
@@ -310,130 +313,131 @@ def get_calendar_date(JD):
     Y         = d -4715-floor((7+M)/10)
     return (Y,M,D)
 
-if __name__=='__main__':
-    import unittest
-    
-    class TestJulian(unittest.TestCase):
-        
-        def test_toJulian(self):
-            self.assertAlmostEqual(2431855.933,get_julian_date(1946,2,4,10.4),places=3)
-            
-        def test_toJulian0(self):
-            self.assertAlmostEqual(0,get_julian_date(-4712,1,1,12),places=3) #noon at start of 4713 BC
-            
-        def test_toCalendar(self):
-            Y,M,D = get_calendar_date(2434903.75)
-            self.assertEqual(1954,Y)
-            self.assertEqual(6,M)
-            self.assertEqual(10.25,D)
-            
-        def test_toCalendar0(self):
-            Y,M,D = get_calendar_date(0)
-            self.assertEqual(-4712,Y)
-            self.assertEqual(1,M)
-            self.assertEqual(1.5,D)        
-            
-    class TestJupiter(unittest.TestCase):
-    
-        # test_mult
-        #
-        # This test uses Murray & Dermott (2.124)
-        def test_compose(self):
-            rotation = compose(omega=radians(14.7392 -100.535 ),I=radians(1.30537),Omega=radians(100.535))
-            self.assertAlmostEqual(0.966839,   rotation[0][0], places=5)
-            self.assertAlmostEqual(-0.254401,  rotation[0][1], places=6)
-            self.assertAlmostEqual(0.0223971,  rotation[0][2], places=6)
-            self.assertAlmostEqual(0.254373,   rotation[1][0], places=5)
-            self.assertAlmostEqual(0.967097,   rotation[1][1], places=6)
-            self.assertAlmostEqual(0.00416519, rotation[1][2], places=6)
-            self.assertAlmostEqual(-0.0227198, rotation[2][0], places=6)
-            self.assertAlmostEqual(0.00167014, rotation[2][1], places=7)
-            self.assertAlmostEqual(0.99974,    rotation[2][2], places=6)
-        
-        # test_mult
-        #
-        # This test uses Murray & Dermott (2.124)            
-        def test_get_XYZ(self):
-            x     = -5.39027
-            y     = -0.818277
-            r     = np.sqrt(x*x + y * y)
-            f     = np.arctan2(y,x)
-            X,Y,Z = get_XYZ(omega=radians(14.7392 -100.535 ),I=radians(1.30537),Omega=radians(100.535),r=r,f=f)
-            self.assertAlmostEqual(-5.00336, X, places=5)
-            self.assertAlmostEqual(-2.16249, Y, places=5)
-            self.assertAlmostEqual(0.121099, Z, places=6)
 
-        def test_get_T(self):
-            self.assertAlmostEqual(-0.06266423,(2449256.189-2451545.0)/36525) # check Julian centuries
-            
-        def test_get_mean_longitude(self):
-            self.assertAlmostEqual(radians(204.234),get_mean_longitude(T=-0.06266423)%(2*np.pi),places=3)
-            
-        def test_get_xy(self):
-            x,y = get_xy(T            = -0.06266423,
-                         lambdaT      = get_mean_longitude(T=-0.06266423),
-                         e            = get_eccentricity(T=-0.06266423),
-                         a            = get_semimajor_axis(-0.06266423),
-                         varpi        = get_longitude_of_perihelion(-0.06266423))
-            self.assertAlmostEqual(-5.39027,x,places=5)
-            self.assertAlmostEqual(-0.818277,y,places=5)
-            
-        def test_eccentricty(self):
-            self.assertAlmostEqual(0.0484007,
-                                   get_eccentricity(T=-0.06266423),places=6)
-            
-        def test_semimajor_axis(self):
-            self.assertAlmostEqual(5.20332,
-                                   get_semimajor_axis(T=-0.06266423),places=4)
-            
-        def test_get_longitude_of_perihelion(self):
-            self.assertAlmostEqual(radians(14.7392),
-                                   get_longitude_of_perihelion(T=-0.06266423),
-                                   places=4)        
-               
-    class TestKepler(unittest.TestCase):
-        # test_kepler_inverse
-        #
-        # Verify solution to Kepler's equation for a range of parameters
-        def test_kepler_inverse(self):
-            N = 25
-            for i in range(N):
-                mean_anomaly = 2 * np.pi /N
-                E            = get_eccentric_anomaly(e=0.205635,M=mean_anomaly)
-                M            = E - 0.205635 * np.sin(E)
-                self.assertAlmostEqual(mean_anomaly,M)
-            
-        # test_kepler_jupiter
-        #
-        # See paragraph before MD 2.123
-        def test_kepler_jupiter(self):
-            self.assertAlmostEqual(radians(189.059),
-                                   get_eccentric_anomaly(
-                                       M=radians(189.495),
-                                       e=0.0484007),
-                                   places=4)
-            
-    class TestOrbit(unittest.TestCase):
-        
-        def test_perihelion(self): # see https://www.timeanddate.com/astronomy/perihelion-aphelion-solstice.html 
-            Xs,Ys,Zs,Ts     = create_orbit(( 1.00000011, 0.01671022, 0.00005, 102.94719, 348.93936, 100.46435),
-                                       lambda_dot = 1293740.63,
-                                       Nr         = 99,
-                                       From       = get_julian_date(2018,12,31),
-                                       To         = get_julian_date(2019,12,31),
-                                       Incr       = 1)
-            
-            solar_distances = [get_distance(Xs[i],Ys[i],Zs[i],0,0,0) for i in range(len(Ts))]
-            
-            minima          = [i for i in range(1,len(solar_distances)-1)
-                               if is_minimum(solar_distances[i-1],
-                                             solar_distances[i],
-                                             solar_distances[i+1])]
-            Y,M,D           = get_calendar_date(Ts[minima[0]])
-            
-            self.assertEqual(1,len(minima))
-            self.assertEqual(2019,Y)
-            self.assertEqual(1,M)
-            self.assertEqual(3.5,D)
+
     
-    unittest.main()
+class TestJulian(TestCase):
+    
+    def test_toJulian(self):
+        self.assertAlmostEqual(2431855.933,get_julian_date(1946,2,4,10.4),places=3)
+        
+    def test_toJulian0(self):
+        self.assertAlmostEqual(0,get_julian_date(-4712,1,1,12),places=3) #noon at start of 4713 BC
+        
+    def test_toCalendar(self):
+        Y,M,D = get_calendar_date(2434903.75)
+        self.assertEqual(1954,Y)
+        self.assertEqual(6,M)
+        self.assertEqual(10.25,D)
+        
+    def test_toCalendar0(self):
+        Y,M,D = get_calendar_date(0)
+        self.assertEqual(-4712,Y)
+        self.assertEqual(1,M)
+        self.assertEqual(1.5,D)        
+        
+class TestJupiter(TestCase):
+
+    # test_mult
+    #
+    # This test uses Murray & Dermott (2.124)
+    def test_compose(self):
+        rotation = compose(omega=np.radians(14.7392 -100.535 ),I=np.radians(1.30537),Omega=np.radians(100.535))
+        self.assertAlmostEqual(0.966839,   rotation[0][0], places=5)
+        self.assertAlmostEqual(-0.254401,  rotation[0][1], places=6)
+        self.assertAlmostEqual(0.0223971,  rotation[0][2], places=6)
+        self.assertAlmostEqual(0.254373,   rotation[1][0], places=5)
+        self.assertAlmostEqual(0.967097,   rotation[1][1], places=6)
+        self.assertAlmostEqual(0.00416519, rotation[1][2], places=6)
+        self.assertAlmostEqual(-0.0227198, rotation[2][0], places=6)
+        self.assertAlmostEqual(0.00167014, rotation[2][1], places=7)
+        self.assertAlmostEqual(0.99974,    rotation[2][2], places=6)
+    
+    # test_mult
+    #
+    # This test uses Murray & Dermott (2.124)            
+    def test_get_XYZ(self):
+        x     = -5.39027
+        y     = -0.818277
+        r     = np.sqrt(x*x + y * y)
+        f     = np.arctan2(y,x)
+        X,Y,Z = get_XYZ(omega=np.radians(14.7392 -100.535 ),I=np.radians(1.30537),Omega=np.radians(100.535),r=r,f=f)
+        self.assertAlmostEqual(-5.00336, X, places=5)
+        self.assertAlmostEqual(-2.16249, Y, places=5)
+        self.assertAlmostEqual(0.121099, Z, places=6)
+
+    def test_get_T(self):
+        self.assertAlmostEqual(-0.06266423,(2449256.189-2451545.0)/36525) # check Julian centuries
+        
+    def test_get_mean_longitude(self):
+        self.assertAlmostEqual(np.radians(204.234),get_mean_longitude(T=-0.06266423)%(2*np.pi),places=3)
+        
+    def test_get_xy(self):
+        x,y = get_xy(T            = -0.06266423,
+                     lambdaT      = get_mean_longitude(T=-0.06266423),
+                     e            = get_eccentricity(T=-0.06266423),
+                     a            = get_semimajor_axis(-0.06266423),
+                     varpi        = get_longitude_of_perihelion(-0.06266423))
+        self.assertAlmostEqual(-5.39027,x,places=5)
+        self.assertAlmostEqual(-0.818277,y,places=5)
+        
+    def test_eccentricty(self):
+        self.assertAlmostEqual(0.0484007,
+                               get_eccentricity(T=-0.06266423),places=6)
+        
+    def test_semimajor_axis(self):
+        self.assertAlmostEqual(5.20332,
+                               get_semimajor_axis(T=-0.06266423),places=4)
+        
+    def test_get_longitude_of_perihelion(self):
+        self.assertAlmostEqual(np.radians(14.7392),
+                               get_longitude_of_perihelion(T=-0.06266423),
+                               places=4)        
+           
+class TestKepler(TestCase):
+    # test_kepler_inverse
+    #
+    # Verify solution to Kepler's equation for a range of parameters
+    def test_kepler_inverse(self):
+        N = 25
+        for i in range(N):
+            mean_anomaly = 2 * np.pi /N
+            E            = get_eccentric_anomaly(e=0.205635,M=mean_anomaly)
+            M            = E - 0.205635 * np.sin(E)
+            self.assertAlmostEqual(mean_anomaly,M)
+        
+    # test_kepler_jupiter
+    #
+    # See paragraph before MD 2.123
+    def test_kepler_jupiter(self):
+        self.assertAlmostEqual(np.radians(189.059),
+                               get_eccentric_anomaly(
+                                   M=np.radians(189.495),
+                                   e=0.0484007),
+                               places=4)
+        
+class TestOrbit(TestCase):
+    
+    def test_perihelion(self): # see https://www.timeanddate.com/astronomy/perihelion-aphelion-solstice.html 
+        Xs,Ys,Zs,Ts     = create_orbit(( 1.00000011, 0.01671022, 0.00005, 102.94719, 348.93936, 100.46435),
+                                   lambda_dot = 1293740.63,
+                                   Nr         = 99,
+                                   From       = get_julian_date(2018,12,31),
+                                   To         = get_julian_date(2019,12,31),
+                                   Incr       = 1)
+        
+        solar_distances = [get_distance(Xs[i],Ys[i],Zs[i],0,0,0) for i in range(len(Ts))]
+        
+        minima          = [i for i in range(1,len(solar_distances)-1)
+                           if is_minimum(solar_distances[i-1],
+                                         solar_distances[i],
+                                         solar_distances[i+1])]
+        Y,M,D           = get_calendar_date(Ts[minima[0]])
+        
+        self.assertEqual(1,len(minima))
+        self.assertEqual(2019,Y)
+        self.assertEqual(1,M)
+        self.assertEqual(3.5,D)
+ 
+if __name__=='__main__':   
+    main()
