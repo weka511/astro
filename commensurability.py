371093eceb98aefa8d65381555056f5b5091ac6b
Simon Crase <simon@greenweaves.nz>
2026-09-08 19:38:48 +1200

    #39 Startwed convertibg code to current standard

--- commensurability.py
+++ commensurability.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2019 Greenweaves Software Limited
+# Copyright (C) 2019-2026 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,48 +15,63 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-import random, matplotlib.pyplot as plt, numpy as np
+from argparse import ArgumentParser
+from pathlib import Path
+from matplotlib.pyplot import figure, show
+import numpy as np
 
 
-def identify_commensurabilities(periods,tolerance=0.001,maxp=10):
-     ratios = sorted([(i,j,periods[i]/periods[j]) for j in range(len(periods)) for i in range(j)],
-                     key=lambda x:x[2])
-     return [(i,j,ratio, p) for p in range(1,maxp+1) for i,j,ratio in ratios if abs(ratio-p/(p+1))<tolerance]
+def identify_commensurabilities(periods, tolerance=0.001, maxp=10):
+     ratios = sorted([(i, j, periods[i] / periods[j]) for j in range(len(periods)) for i in range(j)],
+                     key=lambda x: x[2])
+     return [(i, j, ratio, p) for p in range(1, maxp + 1) for i, j, ratio in ratios if abs(ratio - p / (p + 1)) < tolerance]
 
-def monte_carlo(n,N,target_commensurabilities,T=20,seed=None,maxp=10):
-     _,_,_,target = target_commensurabilities[0]
+
+def monte_carlo(n, N, target_commensurabilities, T=20, seed=None, maxp=10, rng=np.random.default_rng()):
+     _, _, _, target = target_commensurabilities[0]
      counts = []
      ps = []
      for i in range(N):
-          periods            = [random.random()*T for i in range(n)] 
+          periods = [rng.uniform() * T for i in range(n)]
           commensurabilities = identify_commensurabilities(periods)
           counts.append(len(commensurabilities))
-          if len(commensurabilities)==1:
-               _,_,_,p=commensurabilities[0]
+          if len(commensurabilities) == 1:
+               _, _, _, p = commensurabilities[0]
                ps.append(p)
-     
-     n,bins = np.histogram(ps,bins=[x + 0.1 for x in range(maxp+2)])
-     i=0
-     while bins[i]<target:
-          i+=1
-     return n[i-1]/sum(n)
-     
-if __name__=='__main__':
-     import argparse
-     parser = argparse.ArgumentParser(description='Calculate probability for commensurabilities for problem 1.5.')    
-     parser.add_argument('-N',type=int, help='Number of Monte Carlo calculations',default=100)
-     parser.add_argument('--seed','-s',help='Seed for random number generator',default=None)
-     args = parser.parse_args()
-     random.seed(args.seed)
-     
+
+     n, bins = np.histogram(ps, bins=[x + 0.1 for x in range(maxp + 2)])
+     i = 0
+     while bins[i] < target:
+          i += 1
+     return n[i - 1] / sum(n)
+
+
+def parse_args():
+     parser = ArgumentParser(description='Calculate probability for commensurabilities for problem 1.5.')
+     parser.add_argument('-N', type=int, help='Number of Monte Carlo calculations', default=100)
+     parser.add_argument('--seed', '-s', help='Seed for random number generator', default=None)
+     parser.add_argument('--figs', default='./figs', help=f'Path to plots')
+     return parser.parse_args()
+
+
+def main():
+     args = parse_args()
+     rng = np.random.default_rng(args.seed)
+
      with open('data/commensurability.dat') as data:
-          periods            = [float(line.strip()) for line in data]
+          periods = [float(line.strip()) for line in data]
           commensurabilities = identify_commensurabilities(periods)
- 
-          probs = [monte_carlo(len(periods),1000,commensurabilities,seed=None) for i in range(args.N)]
-          plt.figure()
-          plt.hist(probs)
-          plt.title('N={0}, mean= {1:.3f}, std= {2:.3f}'.format(args.N,np.mean(probs),np.std(probs)))
-          
-     plt.show()
-            
+
+          probs = [monte_carlo(len(periods), 1000, commensurabilities, seed=None, rng=rng) for i in range(args.N)]
+          fig = figure()
+          ax = fig.add_subplot(1,1,1)
+          ax.hist(probs)
+          ax.set_title('N={0}, mean= {1:.3f}, std= {2:.3f}'.format(args.N, np.mean(probs), np.std(probs)))
+          fig.savefig(Path(args.figs)/Path(__file__).stem)
+
+     show()
+
+
+if __name__ == '__main__':
+     main()
+
