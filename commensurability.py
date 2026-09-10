756dc856d86e24219ba6f3483cf29e3ef16ae319
Simon Crase <simon@greenweaves.nz>
2026-09-10 12:19:52 +1200

    #39 About to refactor monte-carlo stuff

--- commensurability.py
+++ commensurability.py
@@ -27,27 +27,58 @@ import numpy as np
 __version__ = '1.0'
 __author__ = 'Simon Crase'
 
-def identify_commensurabilities(periods, tolerance=0.001, maxp=10):
+def identify_commensurabilities(periods, atol=0.001, maxp=10):
+     '''
+     Find the ratios that are close to an integer ratio
+     
+     Parameters:
+         periods
+         atol
+         maxp
+     '''
+     def is_close(x,p):
+          '''
+          Verify that a supplied number is within tolerance of a ratio that is defined by an integer
+          
+          Parameters:
+              x
+              p
+          '''
+          return abs(x - p / (p + 1)) < atol
+     
      ratios = sorted([(i, j, periods[i] / periods[j]) for j in range(len(periods)) for i in range(j)],
                      key=lambda x: x[2])
-     return [(i, j, ratio, p) for p in range(1, maxp + 1) for i, j, ratio in ratios if abs(ratio - p / (p + 1)) < tolerance]
+     return [(i, j, ratio, p) for p in range(1, maxp + 1) for i, j, ratio in ratios if is_close(ratio,p)]
 
 
-def monte_carlo(n, N, target_commensurabilities, T=20, seed=None, maxp=10, rng=np.random.default_rng()):
-     _, _, _, target = target_commensurabilities[0]
-     counts = []
-     ps = []
-     for i in range(N):
-          periods = [rng.uniform() * T for i in range(n)]
-          commensurabilities = identify_commensurabilities(periods)
-          counts.append(len(commensurabilities))
-          if len(commensurabilities) == 1:
-               _, _, _, p = commensurabilities[0]
-               ps.append(p)
+def sample(m, N, target, 
+                T=20, maxp=10, rng=np.random.default_rng()):
+     '''
+     Sample possible orbits
+     
+     Parameters:
+         m         Number of periods
+         N         Number of samples
+         target    The commensurabilities identified in dataset
+         T         Upper bound on orbital periods
+         maxp      Largest integer to be considered in ratios p/(p+1)
+         rng       Random number generator
+     '''
+     def sample_single():
+          ps = []
+          for i in range(N):
+               commensurabilities = identify_commensurabilities(rng.uniform(low=0,high=T,size=m))
+               if len(commensurabilities) == 1:
+                    _, _, _, p = commensurabilities[0]
+                    ps.append(p)
+          return ps     
+     _, _, _, target_p = target[0]
+ 
+     ps = sample_single()
 
      n, bins = np.histogram(ps, bins=[x + 0.1 for x in range(maxp + 2)])
      i = 0
-     while bins[i] < target:
+     while bins[i] < target_p:
           i += 1
      return n[i - 1] / sum(n)
 
@@ -62,17 +93,26 @@ def parse_args():
      return parser.parse_args()
 
 def get_data(file_path):
+     '''
+     Load periods from file
+     
+     Parameters:
+         file_path
+     '''
      with open(file_path) as data:
           data_reader = reader(data)
-          return [float(row[0]) for row in data_reader]
+          return np.array([float(row[0]) for row in data_reader])
           
 def main():
      args = parse_args()
      rng = np.random.default_rng(args.seed)
      periods = get_data((Path(args.data)/Path(__file__).stem).with_suffix('.csv'))
      commensurabilities = identify_commensurabilities(periods)
-     probs = [monte_carlo(len(periods), args.N, commensurabilities, seed=None, rng=rng) for i in range(args.N)]
+     probs = [sample(len(periods), args.N, commensurabilities, rng=rng) for i in range(args.N)]
      fig = figure()
+     cc2 = commensurabilities[0][2]
+     cc3 = commensurabilities[0][3]
+     fig.suptitle(f'Closest ratio to {cc2} is {cc3/(cc3+1)} ({cc3})')
      ax = fig.add_subplot(1,1,1)
      ax.hist(probs)
      ax.set_title('N={0}, mean= {1:.3f}, std= {2:.3f}'.format(args.N, np.mean(probs), np.std(probs)))
