eaf574d2770f8dbbf164360fbceebe02a66dde78
Simon Crase <simon@greenweaves.nz>
2026-09-13 09:50:20 +1200

    #45 Partial conversion to numpy

--- rki.py
+++ rki.py
@@ -32,21 +32,30 @@ __author__ = 'Simon Crase'
 class Driver:
     '''
     Use an integrator to solve an ODE to within a specified error 
+    
+    Attributes:
+        integrator  Used for integrating ODE
+        h_minimum   Step size cannot be increased beyond this value
+        h           Initial step size (variable)
+        h_maximum   Step size cannot be increased beyond this value
+        atol        Maximum tolerable error
+        mult        Used to set a lower bound for error (fraction of epsilon)
+        distance    Distance function used to compute error when we solve equations  
     '''
     def __init__(self, integrator, 
                  h_minimum=1.0e-9, h=0.5, h_maximum=1.0, atol=1.0e-9, mult=0.01, 
                  distance=lambda k, k_new: max([abs(a - b) for (a, b) in zip(k, k_new)])):
-        '''Initialize Driver
+        '''
+        Initialize Driver
         
-           Parameters:
-             integrator  Used for integrating ODE
-             h_minimum   Step size cannot be increased beyond this value
-             h           Initial step size (variable)
-             h_maximum   Step size cannot be increased beyond this value
-             atol        Maximum tolerable error
-             mult        Used to set a lower bound for error (fraction of epsilon)
-             distance    Distance function used to compute error when we solve equations
-             
+        Parameters:
+            integrator  Used for integrating ODE
+            h_minimum   Step size cannot be increased beyond this value
+            h           Initial step size (variable)
+            h_maximum   Step size cannot be increased beyond this value
+            atol        Maximum tolerable error
+            mult        Used to set a lower bound for error (fraction of epsilon)
+            distance    Distance function used to compute error when we solve equations       
         '''
         self.integrator = integrator
         self.h_minimum = h_minimum
@@ -57,29 +66,29 @@ class Driver:
         self.distance = distance
 
     def step(self, y):
-        '''Solve for one step, varying step size of appropriate
+        '''
+        Solve for one step, varying step size to keep weeor within bounds
         
         Parameters:
            y        Current value of dependent variable
         '''
         try:
             y1 = self.integrator.step(self.h, y)  # one step estimate of next value
-            # Two half steps
+            
             y11 = self.integrator.step(0.5 * self.h,
-                                       self.integrator.step(0.5 * self.h, y))
-            # Estimate error by comparing the two estimates for next y
-            error = self.integrator.distance(y1, y11)
+                                       self.integrator.step(0.5 * self.h, y)) # Two half steps
+            
+            error = self.integrator.distance(y1, y11) # Estimate error by comparing the two estimates for next y
 
             if error > self.epsilon:  # too large - reduce step size
-                self.h *= (self.epsilon / error)**(1.0 / self.integrator.order)
+                self.h *= (self.epsilon/error)**(1.0/self.integrator.order)
                 return self.step(y)
 
             if error < self.min_epsilon:  # Is our stepsize too small?
                 if error > 0:
-                    self.h *= (self.min_epsilon / error)**(1.0 / self.integrator.order)
+                    self.h *= (self.min_epsilon/error)**(1.0/self.integrator.order)
                 else:
                     self.h *= 2.0
-                # but don't allow step size to go too small
 
                 if self.h > self.h_maximum:
                     self.h = self.h_maximum
@@ -108,22 +117,31 @@ class ImplicitRungeKutta(ABC):
         def __str__(self):
             return repr(self.value)
 
-    def __init__(self, dy, max_iterations, max_iteration_error, order, 
-                 distance=lambda k, k_new: max([abs(a - b) for (a, b) in zip(k, k_new)])):
-        '''Initialize
+    def __init__(self, dy, max_iterations, max_iteration_error, order, a,b,c,
+                 distance=lambda k0, k1: max([abs(a - b) for (a, b) in zip(k0, k1)])
+                 ):
+        '''
+        Initialize
         
         Parameters:
            dy                  Function f for ODE = dy/dx=f(y)
            max_iterations      Maximum number of iterations for solving implicit equations
            max_iteration_error Maximum error we can tolerate when solving implicit equations
            order               Order of solver
-            distance            Distance function used to compute error when we solve equations
+           distance            Distance function used to compute error when we solve equations
+           a
+           b
+           c
         '''
         self.dy = dy
         self.max_iterations = max_iterations
         self.max_iteration_error = max_iteration_error
         self.order = order
         self.distance = distance
+        self.a = a
+        self.b = b
+        self.c = c
+        self.s = len(b)
 
     def step(self, h, y):
         '''
@@ -133,16 +151,13 @@ class ImplicitRungeKutta(ABC):
            h    Step size
            y    Current value of y
         '''
-        k = [[0 for col in y] for row in range(len(self.b))]
+        k = np.zeros((len(self.b),len(y)))
         for i in range(self.max_iterations):
             k_new = self.iterate(h, y, k)
             if min([self.distance(k0, k1) for (k0, k1) in zip(k, k_new)]) < self.max_iteration_error:
                 yy = [y0 for y0 in y]
                 for l in range(len(yy)):
-                    inner_product = 0
-                    for j in range(self.s):
-                        inner_product += self.b[j] * k_new[j][l]
-                    yy[l] += h * inner_product
+                    yy[l] += h * np.dot(self.b,k_new[:,l])
                 return yy
             else:
                 k = k_new
@@ -155,16 +170,15 @@ class ImplicitRungeKutta(ABC):
             h
             y
             k
+            
         assume that each row of the matrix is a single vector a = [[row1,...]] 
         '''
-        result = [[0 for col in y] for row in range(self.s)]
+        result = np.zeros((self.s,len(y)))
         for i in range(self.s):
-            yy = [y0 for y0 in y]
+            yy = y.copy()
             for l in range(len(k[i])):
-                inner_product = 0
-                for j in range(self.s):
-                    inner_product += self.a[i][j] * k[j][l]
-                yy[l] += h * inner_product
+                yy[l] += h * np.dot(self.a[i,:], k[:,l])
+  
             result[i] = self.dy(yy)
         return result
 
@@ -178,23 +192,22 @@ class ImplicitRungeKutta(ABC):
 
 class ImplicitRungeKutta2(ImplicitRungeKutta):
     '''
-    4th order Gauss-Legendred
+    4th order Gauss-Legendre
     '''
-
     def __init__(self, dy, max_iterations, max_iteration_error):
-        super(ImplicitRungeKutta2, self).__init__(dy, max_iterations, max_iteration_error, 4)
-        r3 = np.sqrt(3.0)
-        self.a = [
-            [0.25, 0.25 - r3 / 6.0],
-            [0.25 + r3 / 6.0, 0.25],
-        ]
-        self.b = [
-            0.5, 0.5]
-        self.c = [
-            0.5 - r3 / 6,
-            0.5 + r3 / 6
-        ]
-        self.s = len(self.b)
+        super().__init__(dy, max_iterations, max_iteration_error, 4,
+                np.array([
+                    [0.25, 0.25 - np.sqrt(3.0)/6.0],
+                    [0.25 + np.sqrt(3.0)/6.0, 0.25],
+                ]),
+                np.array([
+                    0.5, 0.5
+                ]),
+                np.array([
+                    0.5 - np.sqrt(3.0)/6,
+                    0.5 + np.sqrt(3.0)/6
+                ]))
+   
 
 
 class ImplicitRungeKutta4(ImplicitRungeKutta):
@@ -203,23 +216,23 @@ class ImplicitRungeKutta4(ImplicitRungeKutta):
     '''
 
     def __init__(self, dy, max_iterations=200, atol=1.0e-18):
-        super(ImplicitRungeKutta4, self).__init__(dy, max_iterations, atol, 6)
-        r15 = np.sqrt(15.0)
-        self.a = [
-            [5.0 / 36.0, 2.0 / 9.0 - r15 / 15.0, 5.0 / 36.0 - r15 / 30.0],
-            [5.0 / 36.0 + r15 / 24.0, 2.0 / 9.0, 5.0 / 36.0 - r15 / 24.0],
-            [5.0 / 36.0 + r15 / 30.0, 2.0 / 9.0 + r15 / 15.0, 5.0 / 36.0]
-        ]
-        self.b = [
-            5.0 / 18.0,
-            4.0 / 9.0,
-            5.0 / 18.0]
-        self.c = [
-            0.5 - r15 / 10.0,
-            0.5,
-            0.5 + r15 / 10.0
-        ]
-        self.s = len(self.b)
+        super().__init__(dy, max_iterations, atol, 6,
+                         np.array([
+                                [5.0/36.0, 2.0/9.0 -np.sqrt(15.0)/15.0, 5.0/36.0 -np.sqrt(15.0)/30.0],
+                                [5.0/36.0 +np.sqrt(15.0)/24.0, 2.0/9.0, 5.0/36.0 -np.sqrt(15.0)/24.0],
+                                [5.0/36.0 +np.sqrt(15.0)/30.0, 2.0/9.0 +np.sqrt(15.0)/15.0, 5.0/36.0]
+                            ]),
+                         np.array([
+                                5.0/18.0,
+                                4.0/9.0,
+                                5.0/18.0
+                            ]),
+                         np.array([
+                                0.5 -np.sqrt(15.0)/10.0,
+                                0.5,
+                                0.5 +np.sqrt(15.0)/10.0
+                            ]))
+
 
 def parse_args():
     '''
@@ -234,7 +247,7 @@ def run_once(ax,rk,):
     driver = Driver(rk, 0.000000001, 0.5, 1.0, 0.000000001)
     try:
         nn = 1000
-        y = [1, 0]
+        y = np.array([1, 0])
         xs = []
         ys = []
         for i in range(nn):
