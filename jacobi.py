6591cdac77899ff76e4c59fd91959ba8a8dab67a
Simon Crase <simon@greenweaves.nz>
2026-09-14 09:20:36 +1200

    #49 Align with coding conventions

--- jacobi.py
+++ jacobi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-# Copyright (C) 2016 Greenweaves Software Pty Ltd
+# Copyright (C) 2016-2026 Greenweaves Software Pty Ltd
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,18 +16,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this software.  If not, see <http://www.gnu.org/licenses/>
 
-import numpy as np, matplotlib.pyplot as plt, math as m 
-from matplotlib.backends.backend_pdf import PdfPages
+'''Plot Zero velocity Surfaces for the Jacobi Integral'''
+
+from argparse import ArgumentParser
+import math
+import sys
+from pathlib import Path
+from time import time
+from matplotlib.pyplot import figure, show
+from matplotlib import rcParams
+from matplotlib.pyplot import cm
+import numpy as np
+
+__version__ = '1.0'
+__author__ = 'Simon Crase'
+
+
+def parse_args():
+    '''
+    Parse command line arguments
+    '''
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--figs', default='./figs', help=f'Path to plots')
+    parser.add_argument('--show', default=False, action='store_true', help='Used to display figure')
+    return parser.parse_args()
+
 
 @np.vectorize
-def jacobi(x,y,n=1,mu2=0.2,Cj=0):
-    n2  = n*n
-    mu1 = 1-mu2
-    x2  = x*x
-    y2  = y*y
-    r1  = m.sqrt((x+mu2)*(x+mu2) + y2)
-    r2  = m.sqrt((x-mu1)*(x-mu1) + y2)
-    return  n2*(x2 + y2) + 2*(mu1/r1 + mu2/r2) - Cj
+def jacobi(x, y, n=1, mu2=0.2, Cj=0):
+    n2 = n * n
+    mu1 = 1 - mu2
+    x2 = x * x
+    y2 = y * y
+    r1 = np.sqrt((x + mu2) * (x + mu2) + y2)
+    r2 = np.sqrt((x - mu1) * (x - mu1) + y2)
+    return n2 * (x2 + y2) + 2 * (mu1 / r1 + mu2 / r2) - Cj
+
 
 def bounds(Z):
     minZ = float('inf')
@@ -38,60 +62,48 @@ def bounds(Z):
                 minZ = z
             if z > maxZ:
                 maxZ = z
-    return (m.floor(minZ),m.ceil(maxZ))     
+    return (math.floor(minZ), math.ceil(maxZ))
 
-def plot_jacobi(fig=1,n=1,mu2=0.2,Cj=3.9,limit=5,origin='lower',pdf_pages=None,close=True): 
-    plt.figure(fig)
-    xlist = np.linspace(-limit, limit+0.001, 100)   
-    ylist = np.linspace(-limit, limit+0.001, 100)
-    X,Y = np.meshgrid(xlist, ylist)   
-    Z = jacobi(X,Y,n,mu2,Cj)
-    (z0,z1) = bounds(Z)
-    
-    levels=list(range(z0,0,10))+list(range(0,z1+1,10))
 
-    ticks = [z0,0,z1]
-    c = plt.pcolormesh(X,Y,Z)
-    cbar = plt.colorbar(c,orientation='vertical',ticks=ticks)
-    cbar.ax.set_yticklabels(['min', '0', 'max']) 
-    CS3=plt.contourf(X, Y, Z, levels, cmap=plt.cm.jet,origin=origin)
-    CS2 = plt.contour(X,Y,Z,levels=[0],colors='w',origin=origin,hold='on',linewidths=(1,))
+def plot_jacobi(fig, n=1, mu2=0.2, Cj=3.9, limit=5, origin='lower'):
+    ax = fig.add_subplot(1, 1, 1)
+    xlist = np.linspace(-limit, limit + 0.001, 100)
+    ylist = np.linspace(-limit, limit + 0.001, 100)
+    X, Y = np.meshgrid(xlist, ylist)
+    Z = jacobi(X, Y, n, mu2, Cj)
+    (z0, z1) = bounds(Z)
+
+    levels = list(range(z0, 0, 10)) + list(range(0, z1 + 1, 10))
+
+    ticks = [z0, 0, z1]
+    c = ax.pcolormesh(X, Y, Z)
+    cbar = fig.colorbar(c, orientation='vertical', ticks=ticks)
+    cbar.ax.set_yticklabels(['min', '0', 'max'])
+    CS3 = ax.contourf(X, Y, Z, levels, cmap=cm.jet, origin=origin)
+    CS2 = ax.contour(X, Y, Z, levels=[0], colors='w', origin=origin, hold='on', linewidths=(1,))
     cbar.add_lines(CS2)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title(r'Zero velocity surfaces for $n={0},\mu_2 = {1},C_j={2}$'.format(n,mu2,Cj))
-    if pdf_pages==None:
-        plt.savefig('Jacobi{0}.png'.format(fig))
-    else:
-        pdf_pages.savefig()
-    if close:
-        plt.close(fig)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title(r'Zero velocity surfaces for $n={0},\mu_2 = {1},C_j={2}$'.format(n, mu2, Cj))
 
 
-if __name__=='__main__':
-    import getopt,sys
-    def usage():
-        print ('Usage: python jacobi.py [-h] [-o pdf_file]')    
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="])
-    except getopt.GetoptError as err:
-        print(err)
-        usage()
-        sys.exit(2)
+def main():
+    args = parse_args()
+    rcParams['text.usetex'] = True
+    start = time()
+    n = 1
+    for (Cj, limit) in zip([3.80465327630637, 3.552, 3.197, 2.84011], [1.5, 2, 2, 1]):
+        fig = figure()
+        plot_jacobi(fig=fig, Cj=Cj, limit=limit)
+        fig.savefig(f'{Path(args.figs) / Path(__file__).stem}-{n}')
+        n += 1
+    elapsed = time() - start
+    minutes = int(elapsed / 60)
+    seconds = elapsed - 60 * minutes
+    print(f'Elapsed Time {minutes} m {seconds:.2f} s')
+    if args.show:
+        show()
 
-    output=None
-    for opt, arg in opts:
-        if opt=='-o' or opt=='--output':
-            output = arg if arg.endswith('pdf') else arg + '.pdf'
-        if opt=='-h' or opt=='--help':
-            usage()
-            sys.exit(0)
-            
-    if output == None:
-        pass
-    else:
-        with PdfPages(output) as pdf_pages:
-            fig=1
-            for (Cj,limit) in zip([3.80465327630637,3.552,3.197,2.84011],[1.5,2,2,1]):    
-                plot_jacobi(fig=fig,Cj=Cj,pdf_pages=pdf_pages,limit=limit)
-                fig+=1        
+
+if __name__ == '__main__':
+    main()
