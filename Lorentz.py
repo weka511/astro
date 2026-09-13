0a05974f144bf53acfae317110050d8e1e5a18f8
Simon Crase <simon@greenweaves.nz>
2026-09-13 19:28:02 +1200

    Closed #46

--- Lorentz.py
+++ Lorentz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2015-2019 Greenweaves Software Limited
+# Copyright (C) 2015-2026 Greenweaves Software Limited
 
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,6 +18,18 @@
 # This tests the ImplicitRungeKutts Inntegrator by calculating the
 # evolution of the Lorentz Attractor
 
+from argparse import ArgumentParser
+from pathlib import Path
+from time import time
+from matplotlib.pyplot import figure, show
+from matplotlib import rcParams
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import rki
+
+__version__ = '1.0'
+__author__ = 'Simon Crase'
+
 class Lorentz:
     def __init__(self,sigma,rho,beta):
         self.sigma = sigma
@@ -25,42 +37,53 @@ class Lorentz:
         self.beta  = beta
         
     def dx(self,x):
-        return [
+        return np.array([
             self.sigma*(x[1]-x[0]),      \
             x[0]*(self.rho-x[2])-x[1],   \
             x[0]*x[1]-self.beta*x[2]
-        ]
+        ])
     
     def __str__(self):
         return 'sigma={0},rho={1}.beta={2}'.format(self.sigma,self.rho,self.beta)
-       
-if __name__=='__main__':
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    import rki
-    
-    nn         = 10000
-    resolution = 1
-    h          = 0.01
-    lorentz    = Lorentz(10,28,8.0/3.0)
-    rk         = rki.ImplicitRungeKutta4(lambda x: lorentz.dx(x),10,0.0000001)
-    v          = [1,0,1]
-    xs         = []
-    ys         = []
-    zs         = []
-    for i in range(nn):
-        v = rk.step(h,v)
-        if i%resolution==0:
-            xs.append(v[0])
-            ys.append(v[1])
-            zs.append(v[2])
+ 
+def parse_args():
+    '''
+    Parse command line arguments
+    '''
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--figs', default='./figs', help=f'Path to plots')
+    parser.add_argument('--show',default=False,action='store_true',help='Used to display figure')
+    parser.add_argument('-n', type=int,default=10000)
+    parser.add_argument('--step',type=float,default=0.01)
+    return parser.parse_args()
+      
+def main():
+    rcParams['text.usetex'] = True
+    start  = time()
+    args = parse_args()
+     
+    lorentz = Lorentz(10,28,8.0/3.0)
+    rk = rki.ImplicitRungeKutta4(lambda x: lorentz.dx(x),10,0.0000001)
+    XYZ = np.zeros((args.n+1,3))
+    XYZ[0,:] = np.array([1,0,1])
+    for i in range(args.n):
+        XYZ[i+1,:]  = rk.step(args.step,XYZ[i,:])
         
-    fig = plt.figure()
+    fig = figure(figsize=(12,12))   
     ax  = fig.add_subplot(111, projection='3d')    
-    ax.scatter(xs, ys, zs,s=1,edgecolors ='face')
+    ax.scatter(XYZ[:,0], XYZ[:,1], XYZ[:,2],s=1,edgecolors ='face')
     
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title(lorentz)
-    plt.show()    
+    fig.savefig(Path(args.figs)/Path(__file__).stem)    
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+    if args.show:
+        show()  
+    
+if __name__=='__main__':
+    main()
