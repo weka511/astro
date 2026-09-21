1326f973a93b1a2eab9cd07da28da1d6d8995c41
Simon Crase <simon@greenweaves.nz>
2026-09-22 09:22:51 +1200

    Closed #56

--- chenciner.py
+++ chenciner.py
@@ -21,7 +21,7 @@ Chenciner choreography using symplectic integrator
 
 from argparse import ArgumentParser
 from csv import reader
-from logging import basicConfig,getLogger,INFO
+from logging import basicConfig,getLogger,INFO,FileHandler,StreamHandler,Formatter
 
 from pathlib import Path
 from time import time,strftime
@@ -43,7 +43,7 @@ def parse_args():
     parser.add_argument('--figs', default='./figs', help=f'Path to plots')
     parser.add_argument('--show',default=False,action='store_true',help='Used to display figure')
     parser.add_argument('--data', default='./data', help=f'Path to data files')
-    parser.add_argument('-N','--Iterations',default=100,type=int,help='Number of steps')
+    parser.add_argument('-n','--n',default=12,type=int,help='Number of orbits')
     parser.add_argument('--freq',default=100,type=int,help='Print progress every freq steps')
     parser.add_argument('--step',type=float,default=0.001)
     parser.add_argument('--logs', default='./logs', help=f'Path to log files')
@@ -77,16 +77,44 @@ def read_data(file_name,dim=2):
                     i += 1
  
         return Q,P,masses
+
+def plot_orbits(R,XY,ax=None):
+    '''
+    Plot orbits
+    
+    Parameters:
+        R
+        XY
+        ax
+    '''
+    ax.scatter(XY[:,0],XY[:,1],c='r',marker='*',s=1)
+    ax.scatter(XY[:,2],XY[:,3],c='g',marker='*',s=1)
+    ax.scatter(XY[:,4],XY[:,5],c='b',marker='*',s=1)
+    # Plot initial condition for all three masses
+    ax.scatter(R[0,0],R[0,1],c='r',marker='+',s=200)
+    ax.scatter(R[1,0],R[1,1],c='g',marker='+',s=200)
+    ax.scatter(R[2,0],R[2,1],c='b',marker='+',s=200)
+    
+def create_logger(path_name):
+    def add_handler(handler,logger,formatter):
+        handler.setLevel(INFO)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)    
+    product = getLogger(__name__)
+    product.setLevel(INFO)
+    formatter = Formatter('%(message)s')
+    add_handler(FileHandler(path_name),product,formatter)
+    add_handler(StreamHandler(),product,formatter)   
+    np.set_printoptions(linewidth=np.nan) # Prevent lines being split when we log numpy arrays
+    return product
+
     
 def main():
     '''
     Read initial values and integrate equations of motion
     '''
     args = parse_args()
-    basicConfig(filename=f'{Path(args.logs)/Path(__file__).stem}{strftime('%Y%m%d%H%M%S')}.log', 
-                level=INFO)
-    logger = getLogger(__name__)
-    np.set_printoptions(linewidth=np.nan) # Prevent lines beig split when we log numpy arrays
+    logger = create_logger(f'{Path(args.logs)/Path(__file__).stem}{strftime('%Y%m%d%H%M%S')}.log')
     rcParams['text.usetex'] = True
     start  = time()
     R,R_dot,m = read_data(Path(args.data)/args.file_name)
@@ -94,38 +122,26 @@ def main():
     y = hamiltonian.create_initial_values(R,R_dot,m)
     integrator = ImplicitRungeKutta4(lambda y: hamiltonian.dH(y), 10, 1e-9)
     driver = Driver(integrator,h=0.1,h_minimum=0.001)
-    XY = np.zeros((args.Iterations+1,6))
+    T = 6.32591398
+    N = int (args.n*T/args.step)    
+    XY = np.zeros((N+1,6))
 
     XY[0,:] = hamiltonian.get_coordinates(y)
-    for i in range(args.Iterations):
+
+    for i in range(N):
         if args.euler:
             dy = hamiltonian.dH(y) 
             y += args.step*dy
-            logger.info (hamiltonian.get_coordinates(y,atol=1.0e-4))
         else:
             y = driver.step(y)
+            
         XY[i+1,:] = hamiltonian.get_coordinates(y,atol=1.0e-4)
         if i%args.freq == 0:
-            print ((f'Step {i}, h={driver.h}'))
-    fig = figure(figsize=(12,12))
-    fig.suptitle(f'{Path(__file__).stem},Iterations={args.Iterations:,}')
-    ax1 = fig.add_subplot(1,1,1,adjustable='box',aspect=1.0)
-    ax1.scatter(XY[0,:],XY[0,:],c='r',s=1)
-    ax1.scatter(XY[1,:],XY[1,:],c='g',s=1)
-    ax1.scatter(XY[2,:],XY[2,:],c='b',s=1)
-    # Plot initial condition for all three masses
-    ax1.scatter(R[0,0],R[0,1],c='r',marker='+',s=200)
-    ax1.scatter(R[1,0],R[1,1],c='g',marker='+',s=200)
-    ax1.scatter(R[2,0],R[2,1],c='b',marker='+',s=200)
+            logger.info ((f'Step {i}, h={driver.h}'))
+    fig = figure(figsize=(8,8))
+    fig.suptitle(f'{Path(__file__).stem}, Orbits={args.n:,}')
     
-    # Now compare with values converted back from the Canonical Coordinates
-    ax1.scatter(XY[0,0],XY[0,1],c='r',marker='x',s=100)   
-    ax1.scatter(XY[0,2],XY[0,3],c='g',marker='x',s=100)
-    ax1.scatter(XY[0,4],XY[0,5],c='b',marker='x',s=100)
-    
-    #ax1.scatter(R[2,0],R[2,1],c='b',marker='+',s=200)
-    
-    #ax1.scatter(XY[-1,0],XY[-1,1],c='r',marker='s',s=100)
+    plot_orbits(R,XY,ax = fig.add_subplot(1,1,1))
  
     fig.tight_layout(h_pad=2)
     fig.savefig(Path(args.figs)/Path(__file__).stem)    
