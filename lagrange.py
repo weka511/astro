a1ceb656d1d77b59ec426215bb7b7a2b9741051e
Simon Crase <simon@greenweaves.nz>
2026-09-24 19:08:54 +1200

    #61 Parameterized mu and removed ad-hoc solver

--- lagrange.py
+++ lagrange.py
@@ -19,8 +19,21 @@
 '''
 Calculate Equilibrium points L1, L2, and L3, of Lagrange configuration of the 3 body problem
 '''
+
+from argparse import ArgumentParser
 import numpy as np
 from scipy.optimize import newton
+
+__version__ = '1.0'
+__author__ = 'Simon Crase'
+
+def parse_args():
+    '''
+    Parse command line arguments
+    '''
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--mu', default=0.2, type=float)
+    return parser.parse_args()
 
 def fn(r, mu):
     '''
@@ -31,35 +44,11 @@ def fn(r, mu):
             - mu/(1 - mu))
 
 
-def solve(r0, r1, mu, f0=None, f1=None, depth=500, eps=1e-12):
-    '''
-    My equation solver
-    '''
-    r = 0.5 * (r0 + r1)
-    f = fn(r, mu)
-    print(depth, r0, r1, r1 - r0, r, f)
-
-    if depth == 0 or r1 - r0 < eps:
-        return r
-    if f0 == None:
-        f0 = fn(r0, mu)
-        f1 = fn(r1, mu)
-
-    s0 = np.copysign(1.0, f0)
-    s1 = np.copysign(1.0, f1)
-    s = np.copysign(1.0, f)
-    if s0 == s:
-        return solve(r, r1, mu, f, f1, depth=depth - 1)
-    if s == s1:
-        return solve(r0, r, mu, f0, f, depth=depth - 1)
-
-
 def get_alpha(mu):
     '''
     Murray & Dermott, (3.75)
     '''
     return (mu/(3*(1 - mu)))**(1 / 3)
-
 
 def get_u(r, mu):
     '''
@@ -69,14 +58,10 @@ def get_u(r, mu):
     r1 = 1 - r
     return mu1 * (1/r1 + r1**2/ 2) + mu * (1/r + r**2/2) - mu1*mu/2
 
-
 def main():
-    alpha = get_alpha(0.2)
-    mu = 0.2
-    r = newton(lambda x:fn(x, mu),alpha-0.1,x1=alpha+0.1,tol=1e-12)
-    r1 = solve(alpha - 0.1, alpha + 0.05, 0.2)
-    print (f'r={r},fn(r, mu)={fn(r, mu)},u={get_u(r, 0.2)}')
-    print (f'r={r1},fn(r, mu)={fn(r1, mu)},u={get_u(r1, 0.2)}')
+    args = parse_args()
+    alpha = get_alpha(args.mu)
+    r = newton(lambda x:fn(x, args.mu),alpha-0.1,x1=alpha+0.1,tol=1e-12)
+    print (f'r={r},fn(r, mu)={fn(r, args.mu)},u={get_u(r, args.mu)}')
   
-
 main()
